67b5978657fa47705d2aa02610820e86d56255f3
dimi <dimi.ghost@gmail.com>
2013-08-12 17:04:27 +0200

    remove PCA over spectral contrast descriptors from extractor
    It does not make sense to apply a separate PCA for each analyzed track,
    as the basis will be different for each track, thus making impossible to make
    comparisons between tehm. We could have applied DCT instead, but opted to
    provided raw spectral contrast descriptor.

--- src/examples/streaming_extractorpostprocess.cpp
+++ src/examples/streaming_extractorpostprocess.cpp
@@ -20,49 +20,11 @@
 #include "streaming_extractorpostprocess.h"
 #include "poolstorage.h"
 #include "algorithmfactory.h"
-#include "essentiamath.h" // for meanFrames and meanVariances
 
 using namespace std;
 using namespace essentia;
 using namespace standard;
 
-void PCA(Pool& pool, const string& nspace) {
-
-  // namespace:
-  string llspace = "lowlevel.";
-  if (!nspace.empty()) llspace = nspace + ".lowlevel.";
-
-  vector<vector<Real> > sccoeffs = pool.value<vector<vector<Real> > >(llspace + "sccoeffs");
-  vector<vector<Real> > scvalleys = pool.value<vector<vector<Real> > >(llspace + "scvalleys");
-
-  Pool poolSc, poolTransformed;
-
-  for (int iFrame = 0; iFrame < (int)sccoeffs.size(); iFrame++) {
-    vector<Real> merged(2*sccoeffs[iFrame].size(), 0.0);
-    for(int i=0, j=0; i<(int)sccoeffs[iFrame].size(); i++, j++) {
-      merged[j++] = sccoeffs[iFrame][i];
-      merged[j] = scvalleys[iFrame][i];
-    }
-    poolSc.add("contrast", merged);
-  }
-
-  Algorithm* pca  = AlgorithmFactory::create("PCA",
-                                             "namespaceIn",  "contrast",
-                                             "namespaceOut", "contrast");
-  pca->input("poolIn").set(poolSc);
-  pca->output("poolOut").set(poolTransformed);
-  pca->compute();
-
-  pool.set(llspace + "spectral_contrast.mean",
-           meanFrames(poolTransformed.value<vector<vector<Real> > >("contrast")));
-  pool.set(llspace + "spectral_contrast.var",
-           varianceFrames(poolTransformed.value<vector<vector<Real> > >("contrast")));
-
-  // remove original data from spectral contrast:
-  pool.remove(llspace + "sccoeffs");
-  pool.remove(llspace + "scvalleys");
-  delete pca;
-}
 
 // Add missing descriptors which are not computed yet, but will be for the
 // final release or during the 1.x cycle. However, the schema need to be
@@ -86,7 +48,5 @@ void PostProcess(Pool& pool, const Pool& options, const string& nspace) {
     if (find(descNames.begin(), descNames.end(), rhythmspace + "rubato_stop") == descNames.end())
       pool.set(rhythmspace + "rubato_stop", vector<Real>(0));
   }
-  // PCA analysis of spectral contrast output:
-  PCA(pool, nspace);
 }
 
