ae60ecb6ac9b3af008615ad350666dbd7c635705
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2013-10-09 20:48:34 +0200

    changed essentia documentation design

--- doc/sphinxdoc/conf.py
+++ doc/sphinxdoc/conf.py
@@ -172,7 +172,7 @@ html_additional_pages.update(essentia_algorithms)
 #html_domain_indices = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
