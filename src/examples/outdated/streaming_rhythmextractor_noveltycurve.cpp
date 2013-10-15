101148f253bae286bd8c11317bf3744e6547d8c7
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2013-10-15 03:38:13 +0200

    better simpler naming for extractors, moved some old extractors to examples/outdated

--- src/examples/outdated/streaming_rhythmextractor_noveltycurve.cpp
+++ src/examples/outdated/streaming_rhythmextractor_noveltycurve.cpp
@@ -608,7 +608,7 @@ int main(int argc, char* argv[]) {
   Pool pool;
 
   cout << "An outdated rhythm extractor (beat tracker, BPM) based on Novelty Curve (2009)." << endl;
-  cout << "NOTE: this beat tracker is outdated (low accuracy compared to the new one), you might want to use streaming_rhythmextractor instead." << endl;
+  cout << "NOTE: this beat tracker is outdated (low accuracy compared to the new one), you might want to use streaming_rhythmextractor_multifeature instead." << endl;
 
   if (argc < 4 && argc != 3) {
     cout << "Error: wrong number of arguments" << endl;
