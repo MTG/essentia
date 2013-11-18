ab2072edab326e4dfc2629c035cffd1146eeb899
Gerard Roma <gerard.roma@upf.edu>
2013-11-18 16:05:54 +0100

    fixing check for rhythm.beats_loudness_band_ratio

--- src/examples/freesound/FreesoundExtractor.cpp
+++ src/examples/freesound/FreesoundExtractor.cpp
@@ -95,21 +95,19 @@ Pool FreesoundExtractor::computeAggregation(Pool& pool){
   aggregator->compute();
 
 
-
   // add descriptors that may be missing due to content
   const Real emptyVector[] = { 0, 0, 0, 0, 0, 0};
   
   int statsSize = int(sizeof(defaultStats)/sizeof(defaultStats[0]));
-  
+
   if(!pool.contains<vector<Real> >("rhythm.beats_loudness"))
-    for (uint i=0; i<statsSize; i++) 
-        poolStats.set(string("rhythm.beats_loudness.")+defaultStats[i],0);   
-    //results.add("rhythm.beats_loudness",emptyArray);
-  if(!pool.contains<vector<Real> >("rhythm.beats_loudness_band_ratio"))
-    for (uint i=0; i<statsSize; i++) 
-      poolStats.set(string("rhythm.beats_loudness_band_ratio.")+defaultStats[i],arrayToVector<Real>(emptyVector));
+    for (uint i=0; i<statsSize; i++)
+        poolStats.set(string("rhythm.beats_loudness.")+defaultStats[i],0);  
 
-
+  if(!pool.contains<vector<vector<Real> > >("rhythm.beats_loudness_band_ratio"))
+    for (uint i=0; i<statsSize; i++) 
+      poolStats.set(string("rhythm.beats_loudness_band_ratio.")+defaultStats[i],
+        arrayToVector<Real>(emptyVector));
 
   delete aggregator;
 
