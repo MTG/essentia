MTG/essentia

commit 622cf5479ad4817aaafbf9ad9e837eec920ffc15

Fixed way oversized buffers

src/algorithms/rhythm/beattrackermultifeature.cpp
@@ -62,11 +62,10 @@ BeatTrackerMultiFeature::BeatTrackerMultiFeature() : AlgorithmComposite(),
   declareOutput(_ticks, 0, "ticks", "the estimated tick locations [s]");
   declareOutput(_confidence, "confidence", "confidence of the beat tracker [0, 5.32]");
 
-  // NB: We want to have the same output stream type as in TempoTapTicks for
-  // consistency. We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push all values
-  // in the output source at once.
-  _ticks.setBufferType(BufferUsage::forLargeAudioStream);
+  // NB: We need to increase buffer size of the output because the
+  // algorithm works on the level of entire track and we need to push
+  // all values in the output source at once.
+  _ticks.setBufferInfo(BufferInfo(131072));
 
 }
 

src/algorithms/rhythm/onsetdetectionglobal.cpp
@@ -467,11 +467,10 @@ OnsetDetectionGlobal::OnsetDetectionGlobal() : AlgorithmComposite() {
 
   _signal >> _poolStorage->input("data"); // attach input proxy
 
-  // NB: We want to have the same output stream type as in OnsetDetection for
-  // consistency. We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push all values
-  // in the output source at once.
-  _onsetDetections.setBufferType(BufferUsage::forLargeAudioStream);
+  // NB: We need to increase buffer size of the output because the
+  // algorithm works on the level of entire track and we need to push
+  // all values in the output source at once.
+  _onsetDetections.setBufferInfo(BufferInfo(131072));
 }
 
 OnsetDetectionGlobal::~OnsetDetectionGlobal() {
@@ -494,7 +493,7 @@ AlgorithmStatus OnsetDetectionGlobal::process() {
   _onsetDetectionGlobal->output("onsetDetections").set(detections);
   _onsetDetectionGlobal->compute();
 
-  for(size_t i=0; i<detections.size(); ++i) {
+  for (size_t i=0; i<detections.size(); ++i) {
     _onsetDetections.push(detections[i]);
   }
   return FINISHED;

src/algorithms/rhythm/rhythmtransform.cpp
@@ -115,7 +115,6 @@ RhythmTransform::RhythmTransform() : AlgorithmComposite() {
 
   declareInput(_poolStorage->input("data"), 1, "melBands","the energy in the melbands");
   declareOutput(_rhythmTransform, 0, "rhythm", "consecutive frames in the rhythm domain");
-  _rhythmTransform.setBufferType(BufferUsage::forLargeAudioStream);
 }
 
 void RhythmTransform::configure() {

src/algorithms/rhythm/tempotapdegara.cpp
@@ -636,11 +636,10 @@ TempoTapDegara::TempoTapDegara() : AlgorithmComposite() {
 
   _onsetDetections >> _poolStorage->input("data"); // attach input proxy
 
-  // NB: We want to have the same output stream type as in TempoTapTicks for
-  // consistency. We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push all values
-  // in the output source at once.
-  _ticks.setBufferType(BufferUsage::forLargeAudioStream);
+  // NB: We need to increase buffer size of the output because the
+  // algorithm works on the level of entire track and we need to push
+  // all values in the output source at once.
+  _ticks.setBufferInfo(BufferInfo(131072));
 }
 
 
@@ -664,7 +663,7 @@ AlgorithmStatus TempoTapDegara::process() {
   _tempoTapDegara->output("ticks").set(ticks);
   _tempoTapDegara->compute();
 
-  for(size_t i=0; i<ticks.size(); ++i) {
+  for (size_t i=0; i<ticks.size(); ++i) {
     _ticks.push(ticks[i]);
   }
   return FINISHED;
@@ -672,4 +671,3 @@ AlgorithmStatus TempoTapDegara::process() {
 
 } // namespace streaming
 } // namespace essentia
-

src/algorithms/tonal/chordsdetection.cpp
@@ -116,14 +116,9 @@ ChordsDetection::ChordsDetection() : AlgorithmComposite() {
   // FIXME: this is just a temporary hack...
   //        the correct way to do this is to have the algorithm output the chords
   //        continuously while processing, which requires a FrameCutter for vectors
-  _chords.setBufferType(BufferUsage::forLargeAudioStream);
-  _strength.setBufferType(BufferUsage::forLargeAudioStream);
-  // Some old buffer settings that were not enough for long audio 
-  //BufferInfo binfo;
-  //binfo.size = 16384;
-  //binfo.maxContiguousElements = 0;
-  //_chords.setBufferInfo(binfo);
-  //_strength.setBufferInfo(binfo);
+  BufferInfo binfo(65536);
+  _chords.setBufferInfo(binfo);
+  _strength.setBufferInfo(binfo);
 
   attach(_pcp, _poolStorage->input("data"));
 }

src/essentia/types.h
@@ -382,6 +382,9 @@ class BufferInfo {
  public:
   int size;
   int maxContiguousElements;
+
+  BufferInfo(int size = 0, int contiguous = 0) :
+    size(size), maxContiguousElements(contiguous) {}
 };
 
 namespace BufferUsage {

commit 2e84cb3247966a27e524ade3b2b5075510fe8006

Fixed buffer problems introduced in commit 622cf54
added BufferUsage::forMultipleFrames buffer type that allows for 8192 contiguous tokens (values output all at once)
For bigger buffers, use source.setBufferInfo(BufferInfo(size, contiguous)); where 'contiguous' should be the maximum number of tokens that need to be output at once

src/algorithms/rhythm/beattrackermultifeature.cpp
@@ -62,11 +62,9 @@ BeatTrackerMultiFeature::BeatTrackerMultiFeature() : AlgorithmComposite(),
   declareOutput(_ticks, 0, "ticks", "the estimated tick locations [s]");
   declareOutput(_confidence, "confidence", "confidence of the beat tracker [0, 5.32]");
 
-  // NB: We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push
-  // all values in the output source at once.
-  _ticks.setBufferInfo(BufferInfo(131072));
-
+  // Need to set the buffer type to multiple frames as all the ticks
+  // are output all at once
+  _ticks.setBufferType(BufferUsage::forMultipleFrames);
 }
 
 void BeatTrackerMultiFeature::createInnerNetwork() {

src/algorithms/rhythm/onsetdetectionglobal.cpp
@@ -462,15 +462,13 @@ OnsetDetectionGlobal::OnsetDetectionGlobal() : AlgorithmComposite() {
   _onsetDetectionGlobal = standard::AlgorithmFactory::create("OnsetDetectionGlobal");
   _poolStorage = new PoolStorage<Real>(&_pool, "internal.signal");
 
-  declareInput(_signal, 1, "signal", "the input signal");   // 1
-  declareOutput(_onsetDetections, 0, "onsetDetections", "the frame-wise values of the detection function"); // 0
+  declareInput(_signal, 1, "signal", "the input signal");
+  declareOutput(_onsetDetections, 0, "onsetDetections", "the frame-wise values of the detection function");
+  // Need to set the buffer type to multiple frames as all the onsets
+  // are output all at once
+  _onsetDetections.setBufferType(BufferUsage::forMultipleFrames);
 
   _signal >> _poolStorage->input("data"); // attach input proxy
-
-  // NB: We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push
-  // all values in the output source at once.
-  _onsetDetections.setBufferInfo(BufferInfo(131072));
 }
 
 OnsetDetectionGlobal::~OnsetDetectionGlobal() {

src/algorithms/rhythm/rhythmtransform.cpp
@@ -115,6 +115,7 @@ RhythmTransform::RhythmTransform() : AlgorithmComposite() {
 
   declareInput(_poolStorage->input("data"), 1, "melBands","the energy in the melbands");
   declareOutput(_rhythmTransform, 0, "rhythm", "consecutive frames in the rhythm domain");
+  _rhythmTransform.setBufferType(BufferUsage::forMultipleFrames);
 }
 
 void RhythmTransform::configure() {

src/algorithms/rhythm/tempotapdegara.cpp
@@ -636,10 +636,9 @@ TempoTapDegara::TempoTapDegara() : AlgorithmComposite() {
 
   _onsetDetections >> _poolStorage->input("data"); // attach input proxy
 
-  // NB: We need to increase buffer size of the output because the
-  // algorithm works on the level of entire track and we need to push
-  // all values in the output source at once.
-  _ticks.setBufferInfo(BufferInfo(131072));
+  // Need to set the buffer type to multiple frames as all the ticks
+  // are output all at once
+  _ticks.setBufferType(BufferUsage::forMultipleFrames);
 }
 
 

src/algorithms/tonal/chordsdetection.cpp
@@ -116,9 +116,10 @@ ChordsDetection::ChordsDetection() : AlgorithmComposite() {
   // FIXME: this is just a temporary hack...
   //        the correct way to do this is to have the algorithm output the chords
   //        continuously while processing, which requires a FrameCutter for vectors
-  BufferInfo binfo(65536);
-  _chords.setBufferInfo(binfo);
-  _strength.setBufferInfo(binfo);
+  // Need to set the buffer type to multiple frames as all the chords
+  // are output all at once
+  _chords.setBufferType(BufferUsage::forMultipleFrames);
+  _strength.setBufferType(BufferUsage::forMultipleFrames);
 
   attach(_pcp, _poolStorage->input("data"));
 }

src/essentia/streaming/phantombuffer.h
@@ -74,6 +74,11 @@ class PhantomBuffer : public MultiRateBuffer<T> {
       buf.maxContiguousElements = 0;
       break;
 
+    case BufferUsage::forMultipleFrames:
+      buf.size = 16384;
+      buf.maxContiguousElements = 8192;
+      break;
+
     case BufferUsage::forAudioStream:
       buf.size = 65536;
       buf.maxContiguousElements = 4096;

src/essentia/types.h
@@ -396,6 +396,7 @@ namespace BufferUsage {
  */
 enum BufferUsageType {
   forSingleFrames,
+  forMultipleFrames,
   forAudioStream,
   forLargeAudioStream
 };