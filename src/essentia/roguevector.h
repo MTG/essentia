960b045193b93d0e8638d79b88f255fe3ae3019a
Stephen Morton <stephen@tungol.org>
2014-11-21 14:37:36 -0800

    Add FreeBSD support

--- src/essentia/roguevector.h
+++ src/essentia/roguevector.h
@@ -98,7 +98,7 @@ void RogueVector<T>::setSize(size_t size) {
 
 
 // Mac implementation
-#elif defined (OS_MAC)
+#elif defined (OS_MAC) || defined(OS_FREEBSD)
 
 // TODO: this is a big hack that relies on clang/libcpp not changing the memory
 //       layout of the std::vector (very dangerous, but works for now...)
