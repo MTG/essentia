0b6774a3920ee3461e6911f19f5a7eed7d878a5d
mariogearth <Mario@cau125672.ca.upf.edu>
2015-02-05 19:09:06 +0100

    Cleaned some comments
    ChordsDetectionBeats

--- src/algorithms/tonal/chordsdetectionbeats.cpp
+++ src/algorithms/tonal/chordsdetectionbeats.cpp
@@ -63,16 +63,10 @@ void ChordsDetectionBeats::compute() {
   Real str; // strength
 
   chords.reserve(int(hpcp.size()/_numFramesWindow)); // 1478/85 = 17
-  //out << "chords.reserve(int(hpcp.size()/_numFramesWindow)); = " << int(hpcp.size()/_numFramesWindow) << endl;
   strength.reserve(int(hpcp.size()/_numFramesWindow));
 
-  //cout << "int(hpcp.size() = " << (int)hpcp.size() << endl; 
-
-  // for (int j=0; j<ticks.size(); j++) {
-  //   cout << "tick " << j << " " << ticks[j] << endl;
-  // } 
   if(ticks.size() < 2) { 
-  throw EssentiaException("Ticks vector should contain at least 2 elements.");
+  	throw EssentiaException("Ticks vector should contain at least 2 elements.");
   } 
 
   Real diffTicks = 0.0f;
@@ -81,9 +75,7 @@ void ChordsDetectionBeats::compute() {
 
   int frameStart=0;
   int frameEnd=0;
-  //cout << "ticks.size() = "<<ticks.size()<< "from 0 to "<< ticks.size()-1 << ", ticks[size-1]"<<ticks[ticks.size()-1]<< endl; 
-  //cout << "hpcp.size() = length of chords output array in the previous version of the code = " <<hpcp.size()<< endl;
-
+  
   for (int i = 0; i < ticks.size()-1; ++i){
 
     diffTicks = ticks[i+1] - ticks[i];
@@ -115,7 +107,7 @@ void ChordsDetectionBeats::compute() {
   } // for
 
   
-}//method
+} //compute
 
 } // namespace standard
 } // namespace essentia
@@ -185,7 +177,6 @@ AlgorithmStatus ChordsDetectionBeats::process() {
     int indexStart = max(0, i - _numFramesWindow/2);
     int indexEnd = min(i + _numFramesWindow/2, (int)hpcp.size());
 
-    //vector<Real> hpcpAverage = medianFrames(hpcp, indexStart, indexEnd);
     vector<Real> hpcpAverage = meanFrames(hpcp, indexStart, indexEnd);
     normalize(hpcpAverage);
 
