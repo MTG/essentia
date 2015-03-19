bbd422b9cbb69cb5da8ac6a271b8b91f207464a9
Jordi Janer <jordi.janer@upf.edu>
2015-03-19 13:40:16 +0100

    draft files for sine model

---
+++ src/algorithms/synthesis/sinemodel.cpp
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2006-2013  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#include "sinemodel.h"
+#include "essentiamath.h"
+
+using namespace essentia;
+using namespace standard;
+
+const char* SineModel::name = "SineModel";
+const char* SineModel::description = DOC("This algorithm computes the sine model without sine tracking.");
+
+void SineModel::compute() {
+
+  const std::vector<Real>& spectrum = _spectrum.get();
+  Real& maxMagFreq = _maxMagFreq.get();
+
+  if (spectrum.size() < 2) {
+    throw EssentiaException("SineModel: input audio spectrum must be larger than 1 element");
+  }
+
+  int index = std::max_element(spectrum.begin(), spectrum.end()) - spectrum.begin();
+
+  // normalize the maximum to the desired frequency range
+  // (be careful not to confuse with the sampling rate which is the double)
+  maxMagFreq = index * (_sampleRate/2.0) / (spectrum.size()-1);
+}
---
+++ src/algorithms/synthesis/sinemodel.h
@@ -0,0 +1,82 @@
+/*
+ * Copyright (C) 2006-2015  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#ifndef ESSENTIA_SINEMODEL_H
+#define ESSENTIA_SINEMODEL_H
+
+#include "algorithm.h"
+
+namespace essentia {
+namespace standard {
+
+class SineModel : public Algorithm {
+
+ private:
+  Input<std::vector<Real> > _spectrum;
+  Output<Real> _maxMagFreq;
+  Real _sampleRate;
+
+ public:
+  SineModel() {
+    declareInput(_spectrum, "spectrum", "the input spectrum (must have more than 1 element)");
+    declareOutput(_maxMagFreq, "maxMagFreq", "the frequency with the largest magnitude [Hz]");
+  }
+
+  void declareParameters() {
+    declareParameter("sampleRate", "the audio sampling rate [Hz]", "(0,inf)", 44100.);
+  }
+
+  void configure() {
+    _sampleRate = parameter("sampleRate").toReal();
+  }
+
+  void compute();
+
+  static const char* name;
+  static const char* description;
+
+};
+
+} // namespace standard
+} // namespace essentia
+
+#include "streamingalgorithmwrapper.h"
+
+namespace essentia {
+namespace streaming {
+
+class SineModel : public StreamingAlgorithmWrapper {
+
+ protected:
+  Sink<std::vector<Real> > _spectrum;
+  Source<Real> _maxMagFreq;
+
+ public:
+  SineModel() {
+    declareAlgorithm("SineModel");
+    declareInput(_spectrum, TOKEN, "spectrum");
+    declareOutput(_maxMagFreq, TOKEN, "maxMagFreq");
+  }
+};
+
+} // namespace streaming
+} // namespace essentia
+
+
+#endif // ESSENTIA_SINEMODEL_H
