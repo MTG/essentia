0ea75c3390560b479824dbd918c64b59b25687da
Sertan Senturk <contact@sertansenturk.com>
2015-09-09 21:00:12 +0200

    Changed the value of minChunkSize to 30

--- src/algorithms/tonal/pitchfilter.h
+++ src/algorithms/tonal/pitchfilter.h
@@ -61,7 +61,7 @@ class PitchFilter : public Algorithm {
   };
 
   void declareParameters() {
-    declareParameter("minChunkSize", "minumum number of frames in non-zero pitch chunks", "[0,inf)",  10);
+    declareParameter("minChunkSize", "minumum number of frames in non-zero pitch chunks", "[0,inf)", 30);
     declareParameter("useAbsolutePitchConfidence", "treat negative pitch confidence values as positive (use with melodia guessUnvoiced=True)", "{true,false}", false);
     declareParameter("confidenceThreshold", "ratio between the average confidence of the most confident chunk and the minimum allowed average confidence of a chunk", "[0,inf)", 36);
   }
