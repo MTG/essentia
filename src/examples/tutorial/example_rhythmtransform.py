86a032d2528291a4b9075fe04809552ca89aa508
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2016-07-04 19:21:53 +0200

    Add comments explaining rhythm transform to python example

--- src/examples/tutorial/example_rhythmtransform.py
+++ src/examples/tutorial/example_rhythmtransform.py
@@ -8,6 +8,20 @@ except:
     print "usage:", sys.argv[0], "<input_file>"
     sys.exit()
 
+
+"""
+Explanation of Rhythm Transform: 
+- Mel bands are computed on frames of the size 8192 with the frames sample rate = sampleRate/hopSize = 22050/1024 = 21.5Hz
+- Rhythm transform frame size is equal to 256 Mel bands frames
+- Output vector is of size 256/2 + 1 = 129.
+- Therefore it represents periodicities over the interval 0Hz (0th bin) to 22050/1024/2 = 10.75Hz (129th bin),
+- Converting to BPM values this corresponds to an interval from 0 BPM to 22050/1024/2 * 60 = 646 BPM
+- Each bin roughly covers 5 BPM
+- 60-200 BPM interval is covered by only 40-12 = 28 bins
+- 120 BPM rougphly corresponds to bin #24
+"""
+
+
 sampleRate   = 22050
 frameSize    = 8192
 hopSize      = 1024
