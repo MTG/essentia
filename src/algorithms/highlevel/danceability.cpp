edd271cdabea910e4963a35703437415021768de
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2016-11-07 16:50:47 +0100

    Remove debug couts

--- src/algorithms/highlevel/danceability.cpp
+++ src/algorithms/highlevel/danceability.cpp
@@ -149,9 +149,6 @@ void Danceability::compute() {
   for (int i=0; i<nFValues - 1; i++) {
     if (F[i+1] != 0.0) {
       danceability += log10(F[i+1] / F[i]) / log10( ((Real)_tau[i+1]+3.0) / ((Real)_tau[i]+3.0));
-      if (log10(F[i+1] / F[i]) < 0) {
-        cout << "DEBUG:" << i <<", " << F[i+1] << ", " << F[i] << endl;
-      }
     }
     else {
       danceability = 0.0;
