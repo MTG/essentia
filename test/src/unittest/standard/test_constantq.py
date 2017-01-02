86dc9b1c7afe461aa45cc7d6e35c1a10b1b7587c
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2017-01-02 18:06:06 +0100

    Fix code formatting in ConstantQ python test

--- test/src/unittest/standard/test_constantq.py
+++ test/src/unittest/standard/test_constantq.py
@@ -18,9 +18,7 @@
 # version 3 along with this program. If not, see http://www.gnu.org/licenses/
 
 
-
 from essentia_test import *
-import math
 import numpy as np
 import os
 
@@ -28,54 +26,50 @@ script_dir = os.path.dirname(__file__)
 
 
 def cvec(l):
-   	return numpy.array(l, dtype='c8')
+    return numpy.array(l, dtype='c8')
+
 
 class TestCQ(TestCase):
 
-    
     def testRandom(self):
         # input is [1, 0, 0, ...] which corresponds to an ConstantQ of constant magnitude 1
-        with open(os.path.join(script_dir,'constantq/CQinput.txt'), 'r') as f:
-        	#read_data = f.read()
-        	data = np.array([], dtype='complex64')
-        	line = f.readline()
-        	while line != '':
-        		re = float(line.split('\t')[0])
-        		im = float(line.split('\t')[1])
-        		data = np.append(data, re + im * 1j)
-        		line = f.readline()
-				
+        with open(os.path.join(script_dir, 'constantq/CQinput.txt'), 'r') as f:
+            #read_data = f.read()
+            data = np.array([], dtype='complex64')
+            line = f.readline()
+            while line != '':
+                re = float(line.split('\t')[0])
+                im = float(line.split('\t')[1])
+                data = np.append(data, re + im * 1j)
+                line = f.readline()
 
-        with open(os.path.join(script_dir,'constantq/QMoutput.txt'), 'r') as u:
-            QMdata_out = np.array([], dtype='complex64')        
+        with open(os.path.join(script_dir, 'constantq/QMoutput.txt'), 'r') as u:
+            QMdata_out = np.array([], dtype='complex64')
             for line in u:
                 re = line.split('+')[0]
                 re = float(re[1:])
                 im = line.split('+')[1]
                 im = float(im[:-3])
                 QMdata_out = np.append(QMdata_out, re + im * 1j)
-       
 
         CQdata = ConstantQ()(cvec(data))
-        QMdata_out = np.array(QMdata_out, dtype='complex64') 
-        
+        QMdata_out = np.array(QMdata_out, dtype='complex64')
 
-        DifferMean = QMdata_out-CQdata  # difference mean
-        DifferMean = ((sum(abs(DifferMean.real))/len(DifferMean))+(sum(abs(DifferMean.imag))/len(DifferMean)))/2 # difference mean
-        DiverPerReal = (sum(((QMdata_out.real-CQdata.real)/QMdata_out.real)*100))/len(QMdata_out.real) #divergence mean percentage 
-        DiverPerImag = (sum(((QMdata_out.imag-CQdata.imag)/QMdata_out.imag)*100))/len(QMdata_out.imag)  #divergence mean percentage 
+        # difference mean
+        DifferMean = QMdata_out-CQdata
+        DifferMean = ((sum(abs(DifferMean.real))/len(DifferMean))+(sum(abs(DifferMean.imag))/len(DifferMean)))/2
+        # divergence mean percentage
+        DiverPerReal = (sum(((QMdata_out.real-CQdata.real)/QMdata_out.real)*100))/len(QMdata_out.real)
+        DiverPerImag = (sum(((QMdata_out.imag-CQdata.imag)/QMdata_out.imag)*100))/len(QMdata_out.imag)
         DiverPer = (DiverPerReal + DiverPerImag) / 2
 
-        
         """
         print 'Divergence mean percentage is : '+str(DiverPer)[:5]+'%'
         print 'Difference Mean is : '+str(DifferMean)[:9]
         """
 
 
-
 suite = allTests(TestCQ)
 
 if __name__ == '__main__':
     TextTestRunner(verbosity=2).run(suite)
-
