3b3b3a90fc5db860ea0bc8aa56a5b2c81364f922
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2017-02-01 18:15:53 +0100

    Add support for SingleVectorString to Pool
    Similarly to already implemented SingleVectorReal

--- src/essentia/pool.cpp
+++ src/essentia/pool.cpp
@@ -39,6 +39,7 @@ void Pool::clear() {
   _poolSingleReal.clear();
   _poolSingleString.clear();
   _poolSingleVectorReal.clear();
+  _poolSingleVectorString.clear();  
 }
 
 void Pool::checkIntegrity() const {
@@ -57,7 +58,6 @@ void Pool::checkIntegrity() const {
   // TODO: check that there exist no descriptors that have child descriptors and
   //       values
 }
-
 
 
 // this implementation makes the assumption that the key 'name' only exists in
@@ -81,6 +81,7 @@ void Pool::remove(const string& name) {
 
   SEARCH_AND_DESTROY(string, SingleString);
   SEARCH_AND_DESTROY(vector<string>, String);
+  SEARCH_AND_DESTROY(vector<string>, SingleVectorString);
   SEARCH_AND_DESTROY(vector<vector<string> >, VectorString);
 
   SEARCH_AND_DESTROY(vector<TNT::Array2D<Real> >, Array2DReal);
@@ -121,6 +122,7 @@ void Pool::removeNamespace(const string& ns) {
 
   SEARCH_AND_DESTROY(string, SingleString);
   SEARCH_AND_DESTROY(vector<string>, String);
+  SEARCH_AND_DESTROY(vector<string>, SingleVectorString);  
   SEARCH_AND_DESTROY(vector<vector<string> >, VectorString);
 
   SEARCH_AND_DESTROY(vector<TNT::Array2D<Real> >, Array2DReal);
@@ -151,6 +153,7 @@ vector<string> Pool::descriptorNames() const {
   ADD_DESC_NAMES(vector<vector<Real> >, VectorReal);
   ADD_DESC_NAMES(string, SingleString);
   ADD_DESC_NAMES(vector<string>, String);
+  ADD_DESC_NAMES(vector<string>, SingleVectorString);  
   ADD_DESC_NAMES(vector<vector<string> >, VectorString);
   ADD_DESC_NAMES(vector<TNT::Array2D<Real> >, Array2DReal);
   ADD_DESC_NAMES(vector<StereoSample>, StereoSample);
@@ -179,6 +182,7 @@ vector<string> Pool::descriptorNames(const std::string& ns) const {
   ADD_DESC_NAMES(vector<vector<Real> >, VectorReal);
   ADD_DESC_NAMES(string, SingleString);
   ADD_DESC_NAMES(vector<string>, String);
+  ADD_DESC_NAMES(vector<string>, SingleVectorString);
   ADD_DESC_NAMES(vector<vector<string> >, VectorString);
   ADD_DESC_NAMES(vector<TNT::Array2D<Real> >, Array2DReal);
   ADD_DESC_NAMES(vector<StereoSample>, StereoSample);
@@ -199,7 +203,8 @@ vector<string> Pool::descriptorNamesNoLocking() const {
                            _poolStereoSample.size() +
                            _poolSingleReal.size()   +
                            _poolSingleString.size() +
-                           _poolSingleVectorReal.size());
+                           _poolSingleVectorReal.size() + 
+                           _poolSingleVectorString.size());
   int i=0;
 
   #define ADD_DESC_NAMES(type, tname)                                          \
@@ -215,9 +220,11 @@ vector<string> Pool::descriptorNamesNoLocking() const {
   ADD_DESC_NAMES(vector<vector<Real> >, VectorReal);
   ADD_DESC_NAMES(string, SingleString);
   ADD_DESC_NAMES(vector<string>, String);
+  ADD_DESC_NAMES(vector<string>, SingleVectorString);
   ADD_DESC_NAMES(vector<vector<string> >, VectorString);
   ADD_DESC_NAMES(vector<TNT::Array2D<Real> >, Array2DReal);
   ADD_DESC_NAMES(vector<StereoSample>, StereoSample);
+
 
   #undef ADD_DESC_NAMES
 
@@ -322,6 +329,8 @@ void Pool::set(const string& name, const type& value, bool validityCheck) {  \
 SPECIALIZE_SET_IMPL(Real, Real)
 SPECIALIZE_SET_IMPL(string, String)
 SPECIALIZE_SET_IMPL(vector<Real>, VectorReal)
+SPECIALIZE_SET_IMPL(vector<string>, VectorString)
+
 
 void Pool::merge(Pool& p, const string& mergeType) {
 
@@ -361,6 +370,7 @@ void Pool::merge(Pool& p, const string& mergeType) {
   MERGE_SINGLE_POOL(Real, SingleReal);
   MERGE_SINGLE_POOL(string, SingleString);
   MERGE_SINGLE_POOL(vector<Real>, SingleVectorReal);
+  MERGE_SINGLE_POOL(vector<string>, SingleVectorString);
 
   // multiple value:
   MERGE_POOL(Real, Real);
@@ -464,6 +474,8 @@ void Pool::mergeSingle(const string& name, const type& value, const string& merg
 SPECIALIZE_MERGE_SINGLE_IMPL(Real, Real)
 SPECIALIZE_MERGE_SINGLE_IMPL(string, String)
 SPECIALIZE_MERGE_SINGLE_IMPL(vector<Real>, VectorReal)
+SPECIALIZE_MERGE_SINGLE_IMPL(vector<string>, VectorString)
+
 
 void Pool::merge(const string& name, const vector<Array2D<Real> >& value, const string& mergeType) {
   /* first check if the pool has ever seen this key before, if it has, we can
@@ -532,7 +544,7 @@ bool Pool::isSingleValue(const string& name) {
   SEARCH_SINGLE(Real, SingleReal);
   SEARCH_SINGLE(vector<Real>, SingleVectorReal);
   SEARCH_SINGLE(string, SingleString);
-
+  SEARCH_SINGLE(vector<string>, SingleVectorString);
 
   #undef SEARCH_SINGLE
   return false;
