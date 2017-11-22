95f409b4d8a49ba65b9939c1f27247de8c053a28
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2017-11-22 16:55:04 +0100

    Check for file reading errors in YamlInput

--- src/algorithms/io/yamlinput.cpp
+++ src/algorithms/io/yamlinput.cpp
@@ -102,7 +102,10 @@ void YamlInput::compute() {
       char* jsonChar = new char[filesize];
       rewind(file);
       // Load to string
-      fread(jsonChar, sizeof(char), filesize, file);
+      size_t result = fread(jsonChar, sizeof(char), filesize, file);
+      if (result != filesize) {
+        throw EssentiaException("YamlInput: error reading the json file");
+      }
       
       string yamlString = JsonConvert(string(jsonChar, filesize)).parseDict();
       yamlString = unescapeJsonString(yamlString);
