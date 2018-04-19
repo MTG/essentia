3c03339f9cccd0751d97268f92765799c930ecbb
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-04-19 11:41:19 +0200

    Improve DOC for ChordsDetectionBeats further

--- src/algorithms/tonal/chordsdetectionbeats.cpp
+++ src/algorithms/tonal/chordsdetectionbeats.cpp
@@ -30,9 +30,9 @@ const char* ChordsDetectionBeats::category = "Tonal";
 const char* ChordsDetectionBeats::description = DOC(
 "This algorithm estimates chords using pitch profile classes on segments between beats. "
 "It is similar to ChordsDetection algorithm, but the chords are estimated on audio segments between each pair "
-"of consecutive beats. Chroma vector from each segment could be obtained as:\n"
+"of consecutive beats. For each segment the estimation is done based on a chroma (HPCP) vector characterizing it, which can be computed by two methods:\n"
 "  - 'interbeat_median', each resulting chroma vector component is a median of all the component values in the segment\n"
-"  - 'starting_beat', chroma vector is sampled from the start of each segment (that is, its starting beat position) using the first frame in each segment. It makes sense if chroma is preliminary smoothed.\n"
+"  - 'starting_beat', chroma vector is sampled from the start of the segment (that is, its starting beat position) using its first frame. It makes sense if chroma is preliminary smoothed.\n"
 "\n"
 "Quality: experimental (algorithm needs evaluation)\n"
 "\n"
