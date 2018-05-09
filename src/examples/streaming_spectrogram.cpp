1d8b38420349bec9503028c96297a293c1f8002a
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-05-09 19:23:20 +0200

    Add new streaming_spectrogram extractor
    Add a new extractor for spectrograms and log-energy melband
    spectrograms. All parameters are hardcoded, but should be done
    configurable in future.

---
+++ src/examples/streaming_spectrogram.cpp
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2006-2016  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#include <iostream>
+#include <essentia/algorithmfactory.h>
+#include <essentia/streaming/algorithms/poolstorage.h>
+#include <essentia/scheduler/network.h>
+#include "credit_libav.h"
+
+using namespace std;
+using namespace essentia;
+using namespace essentia::streaming;
+using namespace essentia::scheduler;
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 3) {
+    cout << "Error: incorrect number of arguments." << endl;
+    cout << "Usage: " << argv[0] << " audio_input json_output" << endl;
+    cout << "Computes spectrogram and mel-band log-energies spectrogram for the input audiofile." << endl;
+    creditLibAV();
+    exit(1);
+  }
+
+  string audioFilename = argv[1];
+  string outputFilename = argv[2];
+
+  essentia::init();
+
+  Pool pool;
+
+  // TODO this should be configurable
+  Real sampleRate = 44100.0;
+  int frameSize = 2048;
+  int hopSize = 1024;
+  std::string format ("json");
+
+  AlgorithmFactory& factory = streaming::AlgorithmFactory::instance();
+
+  Algorithm* audio = factory.create("MonoLoader",
+                                    "filename", audioFilename,
+                                    "sampleRate", sampleRate);
+
+  Algorithm* fc    = factory.create("FrameCutter",
+                                    "frameSize", frameSize,
+                                    "hopSize", hopSize,
+                                    "silentFrames", "noise");
+
+  Algorithm* w     = factory.create("Windowing",
+                                    "type", "blackmanharris62");
+
+  Algorithm* spec  = factory.create("Spectrum");
+  Algorithm* mfcc  = factory.create("MFCC", "numberBands", 40,
+                                    "numberCoefficients", 20);
+  Algorithm* melbands96 = factory.create("MelBands",
+                                         "numberBands", 96,
+                                         "log", true);
+
+  // Audio -> FrameCutter -> Windowing -> Spectrum
+  audio->output("audio") >> fc->input("signal");
+  fc->output("frame") >> w->input("frame");
+  w->output("frame") >> spec->input("frame");
+
+  // Spectrum -> MFCC -> Pool
+  spec->output("spectrum") >> mfcc->input("spectrum");
+  spec->output("spectrum") >> melbands96->input("spectrum");
+
+  mfcc->output("bands") >> NOWHERE; // only store high-res mel bands
+  mfcc->output("mfcc") >> PC(pool, "lowlevel.mfcc");
+  melbands96->output("bands") >> PC(pool, "lowlevel.melbands96");
+
+
+  cout << "Analyzing " << audioFilename << endl;;
+
+  Network n(audio);
+  n.run();
+
+  // write results to file
+  cout << "Writing results to file " << outputFilename << endl;
+
+  standard::Algorithm* output = standard::AlgorithmFactory::create("YamlOutput",
+                                                                   "filename", outputFilename,
+                                                                   "format", format);
+  output->input("pool").set(pool);
+  output->compute();
+
+  n.clear();
+
+  delete output;
+  essentia::shutdown();
+
+  return 0;
+}
