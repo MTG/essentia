6f7742f06af797b429db6889149c7542551b5404
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-05-17 20:05:00 +0200

    Updates to setup.py packaging script for python
    - Automatically detect version from git
    - Require numpy 1.8 or newer
    - Add an option to skip building 3rdparties using an environment
      variable (to speed up future builds on Travis)
    - Some refactoring

--- setup.py
+++ setup.py
@@ -9,17 +9,6 @@ from setuptools.command.install_lib import install_lib
 library = None
 PYTHON = sys.executable
 
-def build_lib():
-    os.system('rm -rf tmp')
-    os.system('mkdir tmp')
-    os.system('./packaging/build_3rdparty_static_debian.sh')
-    os.system('%s waf configure --build-static --static-dependencies \
-               --with-python --prefix=tmp' % PYTHON)
-    os.system('%s waf' % PYTHON)
-    os.system('%s waf install' % PYTHON)
-    return glob.glob('tmp/lib/python*/*-packages/essentia')[0]
-
-
 class EssentiaInstall(install_lib):
     def install(self):
         global library
@@ -32,10 +21,46 @@ class EssentiaInstall(install_lib):
 class EssentiaBuildExtension(build_ext):
     def run(self):
         global library
-        library = build_lib();
+        os.system('rm -rf tmp; mkdir tmp')
+
+        # Ugly hack using an enviroment variable... There's no way to pass a
+        # custom flag to python setup.py bdist_wheel
+        varname = 'ESSENTIA_WHEEL_SKIP_3RDPARTY'
+        if varname in os.environ and os.environ[varname]=='1':
+            print('Skipping building static 3rdparty dependencies (%s=1)' % varname)
+        else:
+            os.system('./packaging/build_3rdparty_static_debian.sh')
+
+        os.system('%s waf configure --build-static --static-dependencies \
+                   --with-python --prefix=tmp' % PYTHON)
+        os.system('%s waf' % PYTHON)
+        os.system('%s waf install' % PYTHON)
+
+        library = glob.glob('tmp/lib/python*/*-packages/essentia')[0]
 
 
-VERSION = open('VERSION', 'r').read().strip('\n')
+def get_git_version():
+    """ try grab the current version number from git"""
+    version = None
+    if os.path.exists(".git"):
+        try:
+            version = os.popen("git describe --always").read().strip()
+        except Exception as e:
+            print(e)
+    return version
+
+
+def get_version():
+    git_version = get_git_version()
+    version = open('VERSION', 'r').read().strip('\n')
+
+    if git_version:
+        version = git_version
+    else:
+        version = open('VERSION', 'r').read().strip('\n')
+
+    return version
+
 
 classifiers = [
     'License :: OSI Approved :: GNU Affero General Public License v3',
@@ -54,12 +79,14 @@ classifiers = [
     'Programming Language :: Python :: 3',
 ]
 
+
+build_requires = ['numpy>=1.8.2', 'six', 'pyyaml']
+
 module = Extension('name', sources=[])
 
 setup(
     name='essentia',
-    version=VERSION,
-    #version='2.1-beta5'
+    version=get_version(),
     description='Library for audio and music analysis, description and synthesis',
     long_description='C++ library for audio and music analysis, description and synthesis, including Python bindings',
     author='Dmitry Bogdanov',
@@ -73,11 +100,8 @@ setup(
     license='AGPLv3',
     platforms='any',
     classifiers=classifiers,
-    install_requires=[
-        'numpy',
-        'six',
-        'pyyaml'
-    ],
+    setup_requires=build_requires,
+    install_requires=build_requires,
     ext_modules=[module],
     cmdclass={
         'build_ext': EssentiaBuildExtension,
