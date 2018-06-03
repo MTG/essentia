933ee2d6c080fbfc0d1cee8eb52203ef3f387dc4
sildeag <gordon@shieldaig.com>
2018-06-02 19:34:16 -0600

    revert to MTG

--- travis/build_wheels.sh
+++ travis/build_wheels.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-PYBIN = "$1"
-WRKDIR = "$2"
-
-if -z ${PYBIN};
-then
-   local PYBIN=/opt/python/cp36-cp36m/bin/
-   local PYBIN_PYTHON=/opt/python/*/bin
-fi
-if -z ${WRKDIR};
-then
-   local WRKDIR = /io
-fi
-
 set -e -x
 
 # Build tools if using original quay.io/pypa/manylinux1_* docker images
@@ -20,22 +7,22 @@ set -e -x
 #/io/travis/build_tools.sh
 
 # Build static 3rdparty dependencies
-${WRKDIR}/packaging/build_3rdparty_static_debian.sh
+/io/packaging/build_3rdparty_static_debian.sh
 
 # Build static libessentia.a library
 # Use Python3.6. CentOS 5's native python is too old...
-#PYBIN=/opt/python/cp36-cp36m/bin/
-cd ${WRKDIR}
+PYBIN=/opt/python/cp36-cp36m/bin/
+cd /io
 "${PYBIN}/python" waf configure --build-static --static-dependencies
 "${PYBIN}/python" waf
 "${PYBIN}/python" waf install
 cd -
 
 # Compile wheels
-for ${PYBIN} in ${PYBIN_PYTHON}; do
+for PYBIN in /opt/python/*/bin; do
 # use older version of numpy for backwards compatibility of its C API
     "${PYBIN}/pip" install numpy==1.8.2
-    ESSENTIA_WHEEL_SKIP_3RDPARTY=1 ESSENTIA_WHEEL_ONLY_PYTHON=1 "${PYBIN}/pip" wheel ${WRKDIR}/ -w wheelhouse/ -v
+    ESSENTIA_WHEEL_SKIP_3RDPARTY=1 ESSENTIA_WHEEL_ONLY_PYTHON=1 "${PYBIN}/pip" wheel /io/ -w wheelhouse/ -v
 done
 
 # Bundle external shared libraries into the wheels
@@ -44,14 +31,14 @@ for whl in wheelhouse/*.whl; do
 # https://github.com/pypa/python-manylinux-demo/issues/7
     if [[ "$whl" != wheelhouse/six* ]];
     then
-        auditwheel repair "$whl" -w ${WRKDIR}/wheelhouse/
+        auditwheel repair "$whl" -w /io/wheelhouse/
     else
-        cp "$whl" ${WRKDIR}/wheelhouse/
+        cp "$whl" /io/wheelhouse/
     fi
 done
 
 # Install packages and test
-for ${PYBIN} in ${PYBIN_PYTHON}; do
-    "${PYBIN}/pip" install essentia --no-index -f ${WRKDIR}/wheelhouse
+for PYBIN in /opt/python/*/bin/; do
+    "${PYBIN}/pip" install essentia --no-index -f /io/wheelhouse
     (cd "$HOME"; ${PYBIN}/python -c 'import essentia; import essentia.standard; import essentia.streaming')
 done
