bc97ab26d67b7d31392052873efa24f3afc5cf1c
sildeag <gordon@shieldaig.com>
2018-06-03 12:19:48 -0600

    fixes

--- travis/build_wheels_test.sh
+++ travis/build_wheels_test.sh
@@ -40,17 +40,20 @@ ${WRKDIR}/packaging/build_3rdparty_static_debian.sh
 # Use Python3.6. CentOS 5's native python is too old...
 #PYBIN=/opt/python/cp36-cp36m/bin/
 cd ${WRKDIR}
-"${PYBIN}/python" waf configure --build-static --static-dependencies
-"${PYBIN}/python" waf
-"${PYBIN}/python" waf install
+for PYBIN in ${PYBIN_PYTHON_MASK}; do
+   "${PYBIN}" waf configure --build-static --static-dependencies
+   "${PYBIN}" waf
+   "${PYBIN}" waf install
+done
+
 # cd - needs OLDPWD set
 #cd -
 
 # Compile wheels
-for ${PIPBIN} in ${PIPBIN_PYTHON_MASK}; do
+for PIPBIN in ${PIPBIN_PYTHON_MASK}; do
 # use older version of numpy for backwards compatibility of its C API
-    "${PIPBIN}/pip" install numpy==1.8.2
-    ESSENTIA_WHEEL_SKIP_3RDPARTY=1 ESSENTIA_WHEEL_ONLY_PYTHON=1 "${PIPBIN}/pip" wheel ${WRKDIR}/ -w wheelhouse/ -v
+    "${PIPBIN}" install numpy==1.8.2
+    ESSENTIA_WHEEL_SKIP_3RDPARTY=1 ESSENTIA_WHEEL_ONLY_PYTHON=1 "${PIPBIN}" wheel ${WRKDIR}/ -w wheelhouse/ -v
 done
 
 # Bundle external shared libraries into the wheels
@@ -66,7 +69,9 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for ${PIPBIN} in ${PIPBIN_PYTHON_MASK}; do
-    "${PIPBIN}/pip" install essentia --no-index -f ${WRKDIR}/wheelhouse
-    (cd "$HOME"; ${PYBIN}/python -c 'import essentia; import essentia.standard; import essentia.streaming')
+for PIPBIN in ${PIPBIN_PYTHON_MASK}; do
+    "${PIPBIN}" install essentia --no-index -f ${WRKDIR}/wheelhouse
+    for PYBIN in ${PYBIN_PYTHON_MASK}; do
+        (cd "$WRKDIR"; ${PYBIN} -c 'import essentia; import essentia.standard; import essentia.streaming')
+    done
 done
