e531f9bf30ec97bca59372404dcb0bc4f17a111c
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-08-14 07:08:17 -0700

    Update and rename scripts for ffmpeg and fftw3 builds on Windows

--- packaging/win32_3rdparty/prepare_fftw3_msvc.sh
+++ packaging/win32_3rdparty/prepare_fftw3_msvc.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
+. ../build_config.sh
 
 pacman -S unzip --noconfirm
 rm -rf tmp
 mkdir tmp; cd tmp
-wget ftp://ftp.fftw.org/pub/fftw/fftw-3.3.3-dll64.zip
-unzip fftw-3.3.3-dll64.zip
+wget ftp://ftp.fftw.org/pub/fftw/$FFTW_VERSION-dll64.zip
+unzip $FFTW_VERSION-dll64.zip
 
 lib /def:libfftw3-3.def
 lib /def:libfftw3f-3.def
@@ -18,7 +19,9 @@ mv libfftw3f-3.lib lib/fftw3f.lib
 mv libfftw3f-3.dll lib/
 
 cd ..
-mkdir -p builds/fftw-3.3.3
-mv tmp/include builds/fftw-3.3.3
-mv tmp/lib builds/fftw-3.3.3
+mkdir -p builds/$FFTW_VERSION
+mv tmp/include builds/$FFTW_VERSION
+mv tmp/lib builds/$FFTW_VERSION
 rm -r tmp
+
+# TODO: create pc file
