e84ef96d098ba49c7bc9bebbc43e36b87b775134
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-08-27 10:54:52 -0700

    Cleanup yaml build script

--- packaging/win32_3rdparty/build_yaml_msys.sh
+++ packaging/win32_3rdparty/build_yaml_msys.sh
@@ -18,16 +18,14 @@ PATH=/mingw64/bin:$PATH
 CFLAGS=-Wl,--output-def=yaml.def ./configure \
     --prefix=$PREFIX \
     --host=x86_64-w64-mingw32 
-#    --enable-shared --disable-static \
 make
 make install
 
 mv $PREFIX/bin/libyaml-0-2.dll $PREFIX/lib/yaml.dll
 rm $PREFIX/lib/libyaml.*
 
-#dlltool $PREFIX/lib/yaml.dll -z yaml.def --export-all-symbols
 lib -machine:X64 -def:src/yaml.def  -out:$PREFIX/lib/yaml.lib
 sed -i 's/^prefix=.*/prefix=\.\.\/packaging\/win32_3rdparty/' $PREFIX/lib/pkgconfig/yaml-0.1.pc
 
-#cd ../..
-#rm -r tmp
+cd ../..
+rm -r tmp
