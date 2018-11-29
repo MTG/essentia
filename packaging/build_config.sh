2b771147363cbcb3a0608a2a4a22f45b1edf17a1
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2018-11-29 19:45:35 +0100

    Fix download link for Qt in packaging

--- packaging/build_config.sh
+++ packaging/build_config.sh
@@ -23,7 +23,7 @@ FFTW_VERSION=fftw-3.3.2
 LIBSAMPLERATE_VERSION=libsamplerate-0.1.8
 LIBYAML_VERSION=yaml-0.1.5
 CHROMAPRINT_VERSION=1.4.3
-QT_SOURCE_URL=http://download.qt-project.org/official_releases/qt/4.8/4.8.6/qt-everywhere-opensource-src-4.8.6.tar.gz
+QT_SOURCE_URL=http://download.qt-project.org/archive/qt/4.8/4.8.6/qt-everywhere-opensource-src-4.8.6.tar.gz
 GAIA_VERSION=2.4.5
 
 FFMPEG_AUDIO_FLAGS="
