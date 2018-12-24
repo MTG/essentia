129331edbba7a4eafcc925abf270cc6ba943fee0
Alia <alia.morsi@aucegypt.edu>
2018-12-24 01:03:31 +0100

    minor changes to adhere to coding standards

--- test/src/unittests/stats/test_histogram.py
+++ test/src/unittests/stats/test_histogram.py
@@ -23,7 +23,7 @@ from essentia_test import *
 class TestHistogram(TestCase):
 
   def testZero(self):
-    histogram, binEdges = Histogram(normalize = "none", maxValue = 1., minValue = 0., numberBins = 10)(zeros(1000))
+    histogram, binEdges = Histogram(normalize="none", maxValue=1., minValue=0., numberBins=10)(zeros(1000))
     self.assertEqualVector(histogram, [1000., 0., 0., 0., 0., 0., 0., 0., 0., 0.])
     self.assertAlmostEqualVector(binEdges, [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0], 0.0001) 
 
@@ -45,9 +45,9 @@ class TestHistogram(TestCase):
     expectedHistogramUnitSum = [ 0.113,  0.087,  0.098,  0.104,  0.114,  0.086,  0.099,  0.088,  0.102,  0.109]  
     expectedHistogramUnitMax = [ 0.99122807,  0.76315789,  0.85964912,  0.9122807 ,  1., 0.75438596,  0.86842105,  0.77192982,  0.89473684,  0.95614035] 
 
-    (outputHistogramNone, outputEdgesNone) = Histogram(normalize = "none", numberBins = 10, minValue = 0., maxValue = 1.)(inputArray)
-    (outputHistogramUnitSum, outputEdgesUnitSum) = Histogram(normalize = "unit_sum", numberBins = 10, minValue = 0., maxValue = 1.)(inputArray)
-    (outputHistogramUnitMax, outputEdgesUnitMax) = Histogram(normalize = "unit_max", numberBins = 10, minValue = 0., maxValue = 1.)(inputArray)
+    (outputHistogramNone, outputEdgesNone) = Histogram(normalize="none", numberBins=10, minValue=0., maxValue=1.)(inputArray)
+    (outputHistogramUnitSum, outputEdgesUnitSum) = Histogram(normalize="unit_sum", numberBins=10, minValue=0., maxValue=1.)(inputArray)
+    (outputHistogramUnitMax, outputEdgesUnitMax) = Histogram(normalize="unit_max", numberBins=10, minValue=0., maxValue=1.)(inputArray)
 
     self.assertAlmostEqualVector(outputEdgesNone, expectedEdges, 0.001)
     self.assertAlmostEqualVector(outputHistogramNone, expectedHistogramNone, 0.001)
