bb78608957beeb1f75c56ee5ceef3d0c91f36454
pabloEntropia <pablo.alonso@upf.edu>
2019-03-18 18:21:17 +0100

    Remove unnecessary Cephes code

--- src/3rdparty/cephes/bessel/bessel.h
+++ src/3rdparty/cephes/bessel/bessel.h
@@ -15,28 +15,28 @@
 
 #define MACHEP 1.11022302462515654042E-16
 
-double chbevl(double x, double array[], int n);
-double hyperg(double a, double b, double x);
-int airy(double x, double *ai, double *aip, double *bi, double *bip);
+// double chbevl(double x, double array[], int n);
+// double hyperg(double a, double b, double x);
+// int airy(double x, double *ai, double *aip, double *bi, double *bip);
 double polevl(double x, double coef[], int N);
 double p1evl(double x, double coef[], int N);
 
-double i0(double x);
-double i0e(double x);
-double i1(double x);
-double i1e(double x);
+// double i0(double x);
+// double i0e(double x);
+// double i1(double x);
+// double i1e(double x);
 
-double k0(double x);
-double k0e(double x);
-double k1(double x);
-double k1e(double x);
+// double k0(double x);
+// double k0e(double x);
+// double k1(double x);
+// double k1e(double x);
 
 double cephes_iv(double nu, double x);
-double jv(double nu, double x);
-double yv(double nu, double x);
-double kn(int n, double x);
+// double jv(double nu, double x);
+// double yv(double nu, double x);
+// double kn(int n, double x);
 
-double gam(double x);
+// double gam(double x);
 double lgam(double x);
 extern int sgngam;
 
