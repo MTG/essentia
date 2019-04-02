b7373af9f26948979129a461853e64bf739d59c9
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2019-04-02 16:34:27 +0200

    More regression unittest in MelBands
    These tests ensure compatibility with Librosa

--- test/src/unittests/spectral/test_melbands.py
+++ test/src/unittests/spectral/test_melbands.py
@@ -63,7 +63,7 @@ class TestMelBands(TestCase):
 
 
     def testZeroSpectrum(self):
-        # Inputting zeros should return zero. Try with different sizes
+        # Inputting zeros should return zero. Try with different sizes.
         size = 1024
         while (size >= 256 ):
             self.assertEqualVector(MelBands()(zeros(size)), zeros(24))
@@ -71,9 +71,10 @@ class TestMelBands(TestCase):
 
 
     def testRegression(self):
-        # Compare to another reference implementation (Librosa)
+        # Compare to another reference implementation (Librosa).
         spectrum = [0.1 * i for i in range(8)] * 128 + [0.5]
 
+        # Test with 'unit_max' normalization.
         mbands = self.InitMelBands(44100, 24, 'htkMel', 'unit_max')(spectrum)
         expected = [ 2.05486465,  2.0094972 ,  2.38035517,  2.79824033,  3.16617682,
                      3.61716034,  4.32444713,  4.80223426,  5.54741135,  6.36711453,
@@ -120,6 +121,55 @@ class TestMelBands(TestCase):
                      7.81450219,  8.84777817, 10.07150674, 11.36904891, 12.87900428,
                     14.61011763, 16.4970834 , 18.72118075, 21.19401982, 24.00157627,
                     27.18831724, 30.76454194, 34.87460463, 39.47271554 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        # Test with 'unit_tri' normalization.
+        mbands = self.InitMelBands(44100, 24, 'htkMel', 'unit_tri')(spectrum)
+        expected = [ 0.01828156, 0.015554  , 0.01602954, 0.01639416, 0.01613853,
+                     0.01604062, 0.01668432, 0.01611929, 0.0162001 , 0.01617688,
+                     0.01636027, 0.01621879, 0.01628412, 0.01622411, 0.01626985,
+                     0.01626829, 0.01622578, 0.01627751, 0.01623505, 0.01626252,
+                     0.01625425, 0.01625289, 0.0162536 , 0.01625583 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        mbands = self.InitMelBands(22050, 24, 'htkMel', 'unit_tri')(spectrum)
+        expected = [ 0.03292591, 0.03222258, 0.03195268, 0.0335673 , 0.0316511 ,
+                     0.03279031, 0.03264817, 0.03255029, 0.03235988, 0.03253848,
+                     0.03253369, 0.0324984 , 0.03249507, 0.03249614, 0.03253573,
+                     0.03249621, 0.03247829, 0.03255564, 0.03248368, 0.03250309,
+                     0.03252018, 0.03250174, 0.03251048, 0.03251005 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        mbands = self.InitMelBands(16000, 24, 'htkMel', 'unit_tri')(spectrum)
+        expected = [ 0.04384827, 0.04568984, 0.04431549, 0.04532766, 0.04457169,
+                     0.04459876, 0.04490439, 0.04484892, 0.04481375, 0.04471778,
+                     0.04496409, 0.04471002, 0.04472244, 0.0448753 , 0.04482888,
+                     0.04475743, 0.04479537, 0.04480653, 0.04480792, 0.04479294,
+                     0.04480243, 0.04480253, 0.04479705, 0.04480846 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        mbands = self.InitMelBands(44100, 24, 'slaneyMel', 'unit_tri')(spectrum)
+        expected = [ 0.01624121, 0.01641104, 0.01630056, 0.01630368, 0.01633617,
+                     0.01615584, 0.01612855, 0.01671312, 0.01571219, 0.01645687,
+                     0.0163448 , 0.0161142 , 0.01631344, 0.01624956, 0.01627376,
+                     0.01624189, 0.01624565, 0.01625017, 0.01626686, 0.01624825,
+                     0.01625317, 0.01625449, 0.01625259, 0.01625604 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        mbands = self.InitMelBands(22050, 24, 'slaneyMel', 'unit_tri')(spectrum)
+        expected = [ 0.03314226, 0.03166796, 0.03346935, 0.03150762, 0.03347334,
+                     0.03166035, 0.03312444, 0.0324344 , 0.0323428 , 0.03237033,
+                     0.03272491, 0.03241717, 0.03262137, 0.03240989, 0.03255801,
+                     0.0324449 , 0.03256378, 0.03249809, 0.03249135, 0.03251497,
+                     0.03250493, 0.03251817, 0.03249752, 0.03251482 ]
+        self.assertAlmostEqualVector(mbands, expected, 1e-5)
+
+        mbands = self.InitMelBands(16000, 24, 'slaneyMel', 'unit_tri')(spectrum)
+        expected = [ 0.04478409, 0.04490864, 0.04481423, 0.04484866, 0.04483231,
+                     0.04480073, 0.04483834, 0.04465683, 0.04474766, 0.04501282,
+                     0.04470051, 0.04468963, 0.04491881, 0.04477331, 0.04478563,
+                     0.04486125, 0.04472868, 0.04482017, 0.0448038 , 0.04480253,
+                     0.04481325, 0.04477503, 0.04481836, 0.04479247 ]
         self.assertAlmostEqualVector(mbands, expected, 1e-5)
 
 
