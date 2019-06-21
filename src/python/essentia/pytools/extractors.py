4e15abfa7b684dd9ecdc656292c25ed0fdf37c18
pabloEntropia <pablo.alonso@upf.edu>
2019-06-21 10:21:30 +0200

    Add batch_melbands_extractor to pytools
    To this end, common batch processing code was factored out

--- src/python/essentia/pytools/extractors.py
+++ src/python/essentia/pytools/extractors.py
@@ -29,7 +29,7 @@ def __subprocess__(cmd):
     General purpose subprocess.
     """
 
-    cmd_str = ' '.join(cmd)
+    cmd_str = ' '.join([str(x) for x in cmd])
     print('Running "{}"...'.format(cmd_str))
 
     child = Popen(cmd, stdout=None, stderr=PIPE)
@@ -39,15 +39,9 @@ def __subprocess__(cmd):
     return rc, cmd_str, stderr.decode("utf-8")
 
 
-def batch_music_extractor(audio_dir, output_dir, generate_log=True,
-                          audio_types=None, profile=None,
-                          store_frames=False, skip_analyzed=False,
-                          format='yaml', jobs=0):
-    """
-        Processes every audio file matching `audio_types` in `audio_dir` with MusicExtractor. The generated
-        .sig yaml/json files are stored in `output_dir` matching the folder structure found in `audio_dir`.
-    """
-
+def __batch_extractor__(audio_dir, output_dir, extractor_cmd, output_extension,
+                        generate_log=True, audio_types=None, skip_analyzed=False,
+                        jobs=0):
     if not audio_types:
         audio_types = ('.WAV', '.AIFF', '.FLAC', '.MP3', '.OGG')
         print("Audio files extensions considered by default: " +
@@ -59,14 +53,13 @@ def batch_music_extractor(audio_dir, output_dir, generate_log=True,
 
     output_dir = os.path.abspath(output_dir)
 
-    if profile:
-        assert os.path.isfile(profile)
-
     if jobs == 0:
         try:
             jobs = cpu_count()
         except NotImplementedError:
-            print("Failed to automatically detect the cpu count, the analysis will try to continue with 4 jobs. For a different behavior change the `job` parameter.")
+            print('Failed to automatically detect the cpu count, '
+                  'the analysis will try to continue with 4 jobs. '
+                  'For a different behavior change the `job` parameter.')
             jobs = 4
 
     # find all audio files and prepare folder structure in the output folder
@@ -75,42 +68,30 @@ def batch_music_extractor(audio_dir, output_dir, generate_log=True,
     skipped_count = 0
     skipped_files = []
     cmd_lines = []
-    for root, dirnames, filenames in os.walk("."):
+    for root, _, filenames in os.walk("."):
         for filename in filenames:
             if filename.upper().endswith(audio_types):
                 audio_file = os.path.relpath(os.path.join(root, filename))
                 audio_file_abs = os.path.join(audio_dir, audio_file)
-                sig_file = os.path.join(output_dir, audio_file)
+                out_file = os.path.join(output_dir, audio_file)
 
                 if skip_analyzed:
-                    if os.path.isfile(sig_file + '.sig'):
+                    if os.path.isfile( '{}.{}'.format(out_file, output_extension)):
                         print("Found descriptor file for " +
                               audio_file + ", skipping...")
                         skipped_files.append(audio_file)
                         skipped_count += 1
                         continue
-                folder = os.path.dirname(sig_file)
+                folder = os.path.dirname(out_file)
                 if not os.path.exists(folder):
                     os.makedirs(folder)
 
                 elif os.path.isfile(folder):
-                    raise EssentiaError('Cannot create directory {} .There exist a file with the same name. Aborting analysis.'.format(folder))
+                    raise EssentiaError('Cannot create directory {}. '
+                                        'There exist a file with the same name. '
+                                        'Aborting analysis.'.format(folder))
 
-                cmd_line = [
-                            sys.executable,
-                            os.path.join(os.path.dirname(__file__), 'extractors/music_extractor.py'), 
-                            audio_file_abs, sig_file,
-                            '--format', format
-                            ]
-
-                if store_frames:
-                    cmd_line += ['--store_frames']
-
-                if profile:
-                    cmd_line += ['--profile', profile]
-
-                cmd_lines.append(cmd_line)
-
+                cmd_lines.append(extractor_cmd + [audio_file_abs, out_file])
 
     # analyze
     errors, oks = 0, 0
@@ -127,27 +108,75 @@ def batch_music_extractor(audio_dir, output_dir, generate_log=True,
             else:
                 errors += 1
 
-    summary = "Analysis done. {} files have been skipped due to errors, {} were processed and {} already existed.".format(
-        errors, oks, skipped_count)
-    print(summary)
 
-    # generate log
-    if generate_log:
-        log = [summary]
+def batch_music_extractor(audio_dir, output_dir, generate_log=True,
+                          audio_types=None, profile=None,
+                          store_frames=False, skip_analyzed=False,
+                          format='yaml', jobs=0):
+    """
+    Processes every audio file matching `audio_types` in `audio_dir` with MusicExtractor.
+    The generated .sig yaml/json files are stored in `output_dir` matching the folder
+    structure found in `audio_dir`.
+    """
 
-        if errors > 0:
-            log += ['Errors:'] + ['"{}"\n{}\n\n'.format(cmd[idx], stderr[idx])
-                                  for idx, i in enumerate(status) if i != 0]
+    extractor_cmd = [sys.executable, os.path.join(os.path.dirname(__file__),
+                                                  'extractors/music_extractor.py'),
+                     '--format', format]
 
-        if oks > 0:
-            log += ['Oks:'] + [cmd[idx]
-                               for idx, i in enumerate(status) if i == 0]
+    if profile:
+        assert os.path.isfile(profile)
+        extractor_cmd += ['--profile', profile]
 
-        if skipped_count > 0:
-            log += ['Skipped files:'] + skipped_files
+    if store_frames:
+        extractor_cmd += ['--store_frames']
 
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
 
-        with open(os.path.join(output_dir, 'log'), 'w') as f:
-            f.write('\n'.join(log))
+    __batch_extractor__(audio_dir, output_dir, extractor_cmd, 'sig', generate_log=generate_log,
+                        audio_types=audio_types, skip_analyzed=skip_analyzed, jobs=jobs)
+
+
+def batch_melbands_extractor(audio_dir, output_dir, generate_log=True,audio_types=None,
+                             skip_analyzed=False, jobs=0, verbose=None, frame_size=None,
+                             hop_size=None, number_bands=None, sample_rate=None,
+                             max_frequency=None, window_type=None, compression_type=None,
+                             normalize=None):
+    """
+        Generates mel bands for every audio file matching `audio_types` in `audio_dir`.
+        The generated .npy files are stored in `output_dir` matching the folder
+        structure found in `audio_dir`.
+    """
+
+    extractor_cmd = [sys.executable, os.path.join(os.path.dirname(__file__),
+                                                  'extractors/melbands_extractor.py')]
+
+    if verbose:
+        extractor_cmd += ['--verbose']
+
+    if normalize:
+        extractor_cmd += ['--normalize']
+
+    if frame_size:
+        extractor_cmd += ['--frame_size', str(frame_size)]
+
+    if hop_size:
+        extractor_cmd += ['--hop_size', str(hop_size)]
+
+    if number_bands:
+        extractor_cmd += ['--number_bands', str(number_bands)]
+
+    if sample_rate:
+        extractor_cmd += ['--sample_rate', str(sample_rate)]
+
+    if max_frequency:
+        extractor_cmd += ['--max_frequency', str(max_frequency)]
+
+    if window_type:
+        extractor_cmd += ['--window_type', str(window_type)]
+
+    if compression_type:
+        extractor_cmd += ['--compression_type', compression_type]
+
+
+    __batch_extractor__(audio_dir, output_dir, extractor_cmd, 'npy',
+                        generate_log=generate_log, audio_types=audio_types,
+                        skip_analyzed=skip_analyzed, jobs=jobs)
---
+++ src/python/essentia/pytools/extractors/melbands_extractor.py
@@ -0,0 +1,119 @@
+# Copyright (C) 2006-2016  Music Technology Group - Universitat Pompeu Fabra
+#
+# This file is part of Essentia
+#
+# Essentia is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation (FSF), either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the Affero GNU General Public License
+# version 3 along with this program. If not, see http://www.gnu.org/licenses/
+
+import argparse
+import os
+
+import numpy as np
+
+from essentia import Pool
+from essentia import run
+
+from essentia.streaming import (MonoLoader, FrameCutter, Windowing, Spectrum,
+                                MelBands, UnaryOperator)
+
+
+def melbands_extractor(args):
+    filename_in = args.audio_file
+    filename_out = args.npy_file
+    verbose = args.verbose
+    force = args.force
+
+    frame_size = args.frame_size
+    hop_size = args.hop_size
+    sample_rate = args.sample_rate
+    min_frequency = args.min_frequency
+    max_frequency = args.max_frequency
+    window_type = args.window_type
+    number_bands = args.number_bands
+    compression_type = args.compression_type
+
+    spectrum_size = frame_size // 2 + 1
+
+    if not force:
+        if os.path.exists(filename_out):
+            if verbose:
+                print('Skipping "{}"'.format(filename_out))
+            return
+
+    pool = Pool()
+
+    loader = MonoLoader(filename=filename_in,
+                        sampleRate=sample_rate)
+    frameCutter = FrameCutter(frameSize=frame_size,
+                              hopSize=hop_size)
+    w = Windowing(type=window_type,
+                  normalized=False)
+    spec = Spectrum(size=frame_size)
+    mels = MelBands(inputSize=spectrum_size,
+                    sampleRate=sample_rate,
+                    numberBands=number_bands,
+                    weighting='linear',
+                    normalize='unit_tri',
+                    lowFrequencyBound=min_frequency,
+                    highFrequencyBound=max_frequency)
+
+    if compression_type.lower() == 'db':
+        shift = UnaryOperator(type='identity')
+        compresssor = UnaryOperator(type='lin2db')
+
+    elif compression_type.lower() == 'log':
+        shift = UnaryOperator(type='identity', scale=1e4, shift=1)
+        compresssor = UnaryOperator(type='log')
+
+    loader.audio >> frameCutter.signal
+    frameCutter.frame >> w.frame >> spec.frame
+    spec.spectrum >> mels.spectrum
+    mels.bands >> shift.array >> compresssor.array >> (pool, 'mel_bands')
+
+    run(loader)
+
+    mel_bands = np.array(pool['mel_bands'])
+
+    np.save(filename_out, mel_bands)
+
+    if verbose:
+        print('Done for "{}"'.format(filename_out))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description = 'Computes mel bands and stores them as a numpy binary file.')
+
+    parser.add_argument('audio_file',
+                        help='audio file name')
+    parser.add_argument('npy_file',
+                        help='output .npy file name')
+    parser.add_argument('--verbose', '-v', action='store_true',
+                        help='whether to print out status to the standard output')
+    parser.add_argument('--force', '-f', action='store_true',
+                        help='whether to recompute if the output file already exists')
+                        help='Target sample rate')
+    parser.add_argument('--frame_size', '-fs', type=int, default=512)
+    parser.add_argument('--hop_size', '-hs', type=int, default=256)
+    parser.add_argument('--number_bands', '-nb', type=int, default=48)
+    parser.add_argument('--sample_rate', '-sr', type=float, default=1.6e4,
+    parser.add_argument('--max_frequency', '-hf', type=float, default=8.e3,
+                        help='Maximum frequqncy')
+    parser.add_argument('--min_frequency', '-lf', type=float, default=0.,
+                        help='Minimum frequqncy')
+    parser.add_argument('--window_type', '-wt', type=str, default='hann',
+                        help='Essentia Windowing type. Cheeck')
+    parser.add_argument('--compression_type', '-ct', type=str, default='dB',
+                        help='dB: 10log10(x). log: log(1 + 10000 * x)')
+
+    melbands_extractor(parser.parse_args())
