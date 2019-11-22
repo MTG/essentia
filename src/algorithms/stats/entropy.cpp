6fc2d393e3cdd6bc0215761a6f494dc269f7a9ae
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2019-11-22 18:03:13 +0100

    Replace depricated std::bind2nd with lambdas in Crest and Entropy

--- src/algorithms/stats/entropy.cpp
+++ src/algorithms/stats/entropy.cpp
@@ -43,7 +43,7 @@ void Entropy::compute() {
         throw EssentiaException("Entropy: array does not contain any values");
     }
     
-    if (find_if(array.begin(), array.end(), bind2nd(less<Real>(), 0)) != array.end()) {
+    if (find_if(array.begin(), array.end(), [](Real value){ return value < 0; }) != array.end()) {
         throw EssentiaException("Entropy: array must not contain negative values");
     }
     
