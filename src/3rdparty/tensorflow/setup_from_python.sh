e48d7e0559fa9a8fb5684bbf4f5ec25a32196c18
pabloEntropia <pablo.alonso@upf.edu>
2019-12-09 18:20:10 +0100

    Add packaging related scripts

---
+++ src/3rdparty/tensorflow/setup_from_python.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+PYTHON=python3
+
+# config
+MODE=python
+CONTEXT=/usr/local/  # run as sudo if the context directoryt is not owned
+
+# setup
+$PYTHON setup_tensorflow.py -m $MODE -c $CONTEXT
