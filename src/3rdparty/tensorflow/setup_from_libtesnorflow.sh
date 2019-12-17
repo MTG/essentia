fd180fd32f0dd9423ff40692a9141d49e28ab824
pabloEntropia <pablo.alonso@upf.edu>
2019-12-17 19:53:43 +0100

    Update Tensorflow version to 1.15.0

--- src/3rdparty/tensorflow/setup_from_libtesnorflow.sh
+++ src/3rdparty/tensorflow/setup_from_libtesnorflow.sh
@@ -7,7 +7,7 @@ PYTHON=python3
 MODE=libtensorflow
 PLATFORM=linux
 CONTEXT=/usr/local/  # run as sudo if the context directory is not owned
-VERSION=1.14.0
+VERSION=1.15.0
 
 # setup
 $PYTHON setup_tensorflow.py -m $MODE -p $PLATFORM -c $CONTEXT -v $VERSION
