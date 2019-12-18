9d85ffa8fbaccd3260a1a66d1c437256f841611e
pabloEntropia <pablo.alonso@upf.edu>
2019-12-18 14:14:05 +0100

    Add --retry flag to curl in deploy_transfer.sh

--- travis/deploy_transfer.sh
+++ travis/deploy_transfer.sh
@@ -2,9 +2,9 @@ set -e -x
 
 cd wheelhouse
 for whl in essentia*.whl; do
-    curl --upload-file "$whl"  https://transfer.sh/"$whl" -w "\n"
+    curl --connect-timeout 10 --retry 5 --upload-file "$whl"  https://transfer.sh/"$whl" -w "\n"
 done
 cd ../dist
 for sdist in essentia*.tar.gz; do
-    curl --upload-file "$sdist"  https://transfer.sh/"$sdist" -w "\n"
+    curl --connect-timeout 10 --retry 5 --upload-file "$sdist"  https://transfer.sh/"$sdist" -w "\n"
 done
