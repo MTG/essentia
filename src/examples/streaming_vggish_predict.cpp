14305af0e3f475b3e7a9648cd214e1b8064dd9d9
pabloEntropia <pablo.alonso@upf.edu>
2020-01-15 20:59:36 +0100

    Add VGGish C++ extractor

---
+++ src/examples/streaming_vggish_predict.cpp
@@ -0,0 +1,105 @@
+/*
+ * Copyright (C) 2006-2020  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#include <iostream>
+#include <essentia/algorithmfactory.h>
+#include <essentia/streaming/algorithms/poolstorage.h>
+#include <essentia/scheduler/network.h>
+#include "credit_libav.h"
+
+using namespace std;
+using namespace essentia;
+using namespace essentia::streaming;
+using namespace essentia::scheduler;
+
+int main(int argc, char* argv[]) {
+
+  if (argc != 4) {
+    cout << "Error: incorrect number of arguments." << endl;
+    cout << "Usage: " << argv[0] << " audio_input model output" << endl;
+    creditLibAV();
+    exit(1);
+  }
+
+  string audioFilename = argv[1];
+  string modelName = argv[2];
+  string outputFilename = argv[3];
+
+  // register the algorithms in the factory(ies)
+  essentia::init();
+
+  Pool pool;
+
+  /////// PARAMS //////////////
+  Real sampleRate = 16000.0;
+
+  AlgorithmFactory& factory = streaming::AlgorithmFactory::instance();
+
+  Algorithm* audio = factory.create("MonoLoader",
+                                    "filename", audioFilename,
+                                    "sampleRate", sampleRate);
+
+  Algorithm* tfp   = factory.create("TensorflowPredictVGGish",
+                                    "graphFilename", modelName);
+
+  /////////// CONNECTING THE ALGORITHMS ////////////////
+  cout << "-------- connecting algos --------" << endl;
+
+  audio->output("audio")     >>  tfp->input("signal");
+  tfp->output("predictions") >>  PC(pool, "probs");
+
+
+  /////////// STARTING THE ALGORITHMS //////////////////
+  cout << "-------- start processing " << audioFilename << " --------" << endl;
+
+  // create a network with our algorithms...
+  Network n(audio);
+  // ...and run it, easy as that!
+  n.run();
+
+  // aggregate the results
+  Pool aggrPool; // the pool with the aggregated MFCC values
+  const char* stats[] = {"mean"};
+
+  standard::Algorithm* aggr = standard::AlgorithmFactory::create("PoolAggregator",
+                                                                 "defaultStats", arrayToVector<string>(stats));
+
+  aggr->input("input").set(pool);
+  aggr->output("output").set(aggrPool);
+  aggr->compute();
+
+  // write results to file
+  cout << "-------- writing results to standard output " << outputFilename << " --------" << endl;
+
+  cout << aggrPool.value<vector<Real> >("probs.mean");
+
+  standard::Algorithm* output = standard::AlgorithmFactory::create("YamlOutput",
+                                                                   "format", "json",
+                                                                   "filename", outputFilename);
+  output->input("pool").set(aggrPool);
+  output->compute();
+
+  n.clear();
+
+  delete aggr;
+  delete output;
+  essentia::shutdown();
+
+  return 0;
+}
