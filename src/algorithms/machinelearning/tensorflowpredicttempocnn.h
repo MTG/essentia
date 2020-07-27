34054098965580da97fea708b0b228fdcbaeec7b
pabloEntropia <pablo.alonso@upf.edu>
2020-07-27 12:58:28 +0200

    Improve parameter description

--- src/algorithms/machinelearning/tensorflowpredicttempocnn.h
+++ src/algorithms/machinelearning/tensorflowpredicttempocnn.h
@@ -59,7 +59,7 @@ class TensorflowPredictTempoCNN : public AlgorithmComposite {
     declareParameter("input", "the name of the input node in the TensorFlow graph", "", "input");
     declareParameter("output", "the name of the node from which to retrieve the output tensors", "", "output");
     declareParameter("patchHopSize", "the number of frames between the beginnings of adjacent patches. 0 to avoid overlap", "[0,inf)", 128);
-    declareParameter("lastPatchMode", "what to do with the last frames. Options are to `repeat` them to fill the last patch or to `discard` them", "{discard,repeat}", "discard");
+    declareParameter("lastPatchMode", "what to do with the last frames: `repeat` them to fill the last patch or `discard` them", "{discard,repeat}", "discard");
     declareParameter("batchSize", "number of patches to process in parallel. Use -1 to accumulate all the patches and run a single TensorFlow session at the end of the stream.", "[-1,inf)", 1);
   }
 
@@ -109,7 +109,7 @@ class TensorflowPredictTempoCNN : public Algorithm {
     declareParameter("input", "the name of the input nodes in the Tensorflow graph", "", "input");
     declareParameter("output", "the name of the node from which to retrieve the output tensors", "", "output");
     declareParameter("patchHopSize", "number of frames between the beginnings of adjacent patches. 0 to avoid overlap", "[0,inf)", 128);
-    declareParameter("lastPatchMode", "what to do with the last frames. Options are to `repeat` them to fill the last patch or to discard them", "{discard,repeat}", "discard");
+    declareParameter("lastPatchMode", "what to do with the last frames. `repeat` them to fill the last patch or `discard` them", "{discard,repeat}", "discard");
     declareParameter("batchSize", "number of patches to process in parallel. Use -1 to accumulate all the patches and run a single TensorFlow session at the end of the stream.", "[-1,inf)", 1);
   }
 
