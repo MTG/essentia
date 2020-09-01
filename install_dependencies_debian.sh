74e23d21e5d233201a32e12f1176de868b71e4ca
Hobson Lane <hobson@tangibleai.com>
2020-08-31 17:09:24 -0700

    dependencies must be installed before running `waf --configure`

---
+++ install_dependencies_debian.sh
@@ -0,0 +1,3 @@
+sudo apt install -y libavcodec-dev libeigen3-dev libavformat-dev libavutil-dev libavresample-dev libsamplerate0-dev libyaml-dev libtaglib-cil* libtag1-dev python3-taglib libfftw3-* libchromaprint-*
+# https://github.com/supermihi/pytaglib
+pip install pytaglib
