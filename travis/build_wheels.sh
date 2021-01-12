MTG/essentia

commit 733e8e76dd83d7cafa6686f10af61c98edd9f9be

Set numpy ABI version to use with Python 3.9

travis/build_wheels.sh
@@ -80,6 +80,8 @@ for PYBIN in /opt/python/cp3*/bin; do
         NUMPY_VERSION=1.8.2
 
         # Python 3.x
+        if [[ $PYBIN == *"cp39"* ]]; then
+            NUMPY_VERSION=1.19.3
         if [[ $PYBIN == *"cp38"* ]]; then
             NUMPY_VERSION=1.17.4
         elif [[ $PYBIN == *"cp37"* ]]; then

commit 0d8a76f7a61225ba4a43b9729d9af33f115c20cc

Fix broken bash script
Broken after previous commit 733e8e76dd83d7cafa6686f10af61c98edd9f9be

travis/build_wheels.sh
@@ -82,7 +82,7 @@ for PYBIN in /opt/python/cp3*/bin; do
         # Python 3.x
         if [[ $PYBIN == *"cp39"* ]]; then
             NUMPY_VERSION=1.19.3
-        if [[ $PYBIN == *"cp38"* ]]; then
+        elif [[ $PYBIN == *"cp38"* ]]; then
             NUMPY_VERSION=1.17.4
         elif [[ $PYBIN == *"cp37"* ]]; then
             NUMPY_VERSION=1.14.5