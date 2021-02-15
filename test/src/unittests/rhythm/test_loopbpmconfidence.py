b9571d857c3e3943c78a2b7ee107ccc91ae2a74d
cvf-bcn-gituser <c.v.forkin@gmail.com>
2021-02-15 11:07:17 +0100

    Updated tests on audio subsections

--- test/src/unittests/rhythm/test_loopbpmconfidence.py
+++ test/src/unittests/rhythm/test_loopbpmconfidence.py
@@ -32,42 +32,47 @@ class TestLoopBpmConfidence(TestCase):
         """
          Confidence figures are obtained from previous runs algorithm as of
          code baseline on 10-Feb-2021
-         In keeping with regression tests principles, no future changes of algorithm
+         In keeping with regression tests principles, no future changes of the algorithm
          should break these tests.
-         Test for different BPMs starting with correct one(125),
+         Test for different BPMs starting with correct one: 125,
          and check the confidence levels obtained from previous runs.
-         The 3rd param in assertAlmostEqual() has been optimised to 0.02.
+         The 3rd param in assertAlmostEqual() has been optimised to 0.01.
+         We chose this reasonable margin since the expected confidence benchmark 
+         has been rounded to two places of decimal.
         """        
 
         bpmEstimate = 125
         expectedConfidence = 1
         confidence = LoopBpmConfidence()(audio, bpmEstimate)        
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02)
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01)
 
         bpmEstimate = 120
         expectedConfidence = 0.87
         confidence = LoopBpmConfidence()(audio, bpmEstimate)        
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02)
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01)
 
         bpmEstimate = 70
         expectedConfidence = 0.68
         confidence = LoopBpmConfidence()(audio, bpmEstimate)                
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02)
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01)
 
         """
-         Apply same principle above to Sub-Sections of Audio.       
+         Apply same principle above but now to Sub-Sections of Audio,
+         truncating a bit at the end
          The following confidence measurements were taken on 11 Feb. 2021
          for a given set of parameters on different audio subsections.
-         A segment of techno_loop gives a non-ideal estimation of BPM.
-         
-         Subsection [1000:6000], (Randomly chosen points, section 1)
-         BPM Estimate: 125: Confidence: 0.5275888442993164
-         BPM Estimate: 120: Confidence: 0.5464853048324585
-         BPM Estimate: 70: Confidence: 0.7354497313499451
-         Subsection [22000:38000], (Randomly chosen points, section 2)
-         BPM Estimate: 125: Confidence: 0.5117157697677612
-         BPM Estimate: 120: Confidence: 0.4512471556663513
-         BPM Estimate: 70: Confidence: 0.15343916416168213
+         A segment of techno_loop gives a non-ideal estimation of BPM
+         so we expect the confidence to drop.
+       
+         Subsection [0:len90]
+         BPM Estimate: 125: Confidence: 0.19992440938949585
+         BPM Estimate: 120: Confidence: 0.4305669069290161
+         BPM Estimate: 70: Confidence: 0.5011640191078186
+
+         Subsection [len01:len90],
+         BPM Estimate: 125: Confidence: 0.9199735522270203
+         BPM Estimate: 120: Confidence: 0.3631746172904968
+         BPM Estimate: 70: Confidence: 0.7951852083206177
          The ASSERTs will check for these to two places of decimal
         
          These checks act as integrity checks. If any future changes break these asserts,
@@ -75,44 +80,42 @@ class TestLoopBpmConfidence(TestCase):
          The 3rd param in assertAlmostEqual() has been optimised, lowest possible value chosen.
         """
 
-        # Randomly chosen points, Section 1
-        samplePoint1 = 1000
-        samplePoint2 = 6000
+        # Define Markers for significant meaningful subsections 
+        # consistent with sampling rate
+        len90 = int(0.9*len(audio))  # End point for 90% loop and subsection        
+        len01 = int(0.01*len(audio)) # Begin point for subsection loop 
 
+        # Truncated Audios 90%
         bpmEstimate = 125
-        expectedConfidence = 0.53
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)                
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02) 
+        expectedConfidence = 0.2 # rounded from measured 0.19992440938949585
+        confidence = LoopBpmConfidence()(audio[0:len90], bpmEstimate)              
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01) 
 
         bpmEstimate = 120
-        expectedConfidence = 0.55
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)                
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02) 
+        expectedConfidence = 0.43 # rounded from measured 0.4305669069290161
+        confidence = LoopBpmConfidence()(audio[0:len90], bpmEstimate)                          
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01) 
         
         bpmEstimate = 70
-        expectedConfidence = 0.74
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)            
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02) 
-
-        # Randomly chosen points, Section 2
-        samplePoint1 = 22000
-        samplePoint2 = 38000
-        expectedEstimate = 100
+        expectedConfidence = 0.50 # rounded from measured 0.5011640191078186
+        confidence = LoopBpmConfidence()(audio[0:len90], bpmEstimate)                     
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01)
         
+        # Truncated Audios 90% and 1% at beginning
         bpmEstimate = 125
-        expectedConfidence = 0.51
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)                
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02) 
+        expectedConfidence = 0.92 # rounded from measured 0.9199735522270203
+        confidence = LoopBpmConfidence()(audio[len01:len90], bpmEstimate)                         
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01) 
 
         bpmEstimate = 120
-        expectedConfidence = 0.45
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)                
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.02) 
+        expectedConfidence =  0.36 # rounded from measured 0.3631746172904968
+        confidence = LoopBpmConfidence()(audio[len01:len90], bpmEstimate)           
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01) 
         
-        bpmEstimate = 70
-        expectedConfidence = 0.15
-        confidence = LoopBpmConfidence()(audio[samplePoint1:samplePoint2], bpmEstimate)            
-        self.assertAlmostEqual(expectedConfidence, confidence, 0.05) 
+        bpmEstimate = 70 
+        expectedConfidence =  0.79 # rounded from measured 0.7951852083206177
+        confidence = LoopBpmConfidence()(audio[len01:len90], bpmEstimate)
+        self.assertAlmostEqual(expectedConfidence, confidence, 0.01) 
         
     def testEmpty(self):
         # Zero estimate check results in zero confidence
@@ -128,25 +131,27 @@ class TestLoopBpmConfidence(TestCase):
         self.assertEquals(0, confidence)
 
     def testZero(self):
-        zeroAudio = zeros(1000)
+        audio = MonoLoader(filename=join(testdata.audio_dir, 'recorded', 'techno_loop.wav'))() 
+        zeroAudio = zeros(len(audio))
         bpmEstimate = 0
         confidence = LoopBpmConfidence()(zeroAudio, bpmEstimate)
         self.assertEquals(0, confidence)
 
         # Non-zero estimate check results in non-zero confidence
-        # FIX-ME: Maybe given contant zero input, we should expect zero confidence
+        # FIXME: Maybe given contant zero input, we should expect zero confidence
         bpmEstimate = 125
         confidence = LoopBpmConfidence()(zeroAudio, bpmEstimate)
         self.assertNotEquals(0, confidence)
          
     def testConstantInput(self):
-        onesAudio = ones(100)
+        audio = MonoLoader(filename=join(testdata.audio_dir, 'recorded', 'techno_loop.wav'))() 
+        onesAudio = ones(len(audio))
         bpmEstimate = 0
         confidence = LoopBpmConfidence()(onesAudio, bpmEstimate)
         self.assertEquals(0, confidence)
 
         # Non-zero estimate check results in non-zero confidence
-        # FIX-ME: Maybe given contant input, we should expect zero confidence
+        # FIXME: Maybe given contant input, we should expect zero confidence
         bpmEstimate = 125
         confidence = LoopBpmConfidence()(onesAudio, bpmEstimate)
         self.assertNotEquals(0, confidence)
