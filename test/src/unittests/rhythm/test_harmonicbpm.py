086aa77c1552a674b8cc57789b1c721ad97d9ee1
cvf-bcn-gituser <c.v.forkin@gmail.com>
2021-03-10 13:03:51 +0100

    improved range of regression tests

--- test/src/unittests/rhythm/test_harmonicbpm.py
+++ test/src/unittests/rhythm/test_harmonicbpm.py
@@ -44,22 +44,43 @@ class TestHarmonicBpm(TestCase):
         testBpms = zeros(100)
         self.assertRaises(EssentiaException, lambda: HarmonicBpm()(testBpms))
 
-    # Do a regression test on a tempo impulse that varies a bit
-    def testRegression(self):
+    # Perform a series of 4 RTs that vary parameters
+    def testRegressionStandard(self):
         testBpms = [100, 101, 102, 103, 104]
         harmonicBpms = HarmonicBpm(bpm=100)(testBpms)
         expectedBpm = 100
         self.assertEqual(harmonicBpms, expectedBpm)
 
+    # Check the value below threshold is not included in harmonicBpms
+    def testRegressionThresholdLimit(self):
+        testBpms = [99, 100, 101, 102, 103, 104, 250]
+        harmonicBpms = HarmonicBpm(bpm=100, threshold=99)(testBpms)
+        expectedBpm = 100
+        self.assertEqual(harmonicBpms, expectedBpm)
+
+    # Reduce tolerance parameter
+    def testRegressionLowTolerance(self):
+        testBpms = [100, 101, 102, 103, 104]
+        harmonicBpms = HarmonicBpm(bpm=100, tolerance=1)(testBpms)
+        expectedBpm = [100., 101.]
+        self.assertEqualVector(harmonicBpms, expectedBpm)
+
+    # Check for bpm outside of input range
+    def testRegressionBpmHigh(self):
+        testBpms = [100, 101, 102, 103, 104]
+        harmonicBpms = HarmonicBpm(bpm=105)(testBpms)    
+        expectedBpm =  104
+        self.assertEqual(harmonicBpms, expectedBpm)
+
     # Do a regression test on a tempo impulse that varies a bit, for 2 octaves
     def testRegressionMultipleOctave(self):
-        testBpms = [100, 101, 102, 103, 104, 200, 202, 204, 206, 208]
-        harmonicBpms = HarmonicBpm(bpm=100)(testBpms)
-        expectedBpm =[100.0, 200.0]
+        testBpms = [100, 101, 102, 103, 104, 200, 202, 204, 206, 208, 300, 302, 304, 306, 308]
+        harmonicBpms = HarmonicBpm(bpm=100)(testBpms)   
+        expectedBpm =[100.0, 200.0, 300.0]
         self.assertEqualVector(harmonicBpms, expectedBpm)
 
     def testEmpty(self):
-        # nothing should be computed and the resulting pool be empty
+        # nothing should be computed and the resulting pool should be empty
         harmonicBpms = HarmonicBpm(bpm=100)([])
         self.assertEqualVector(harmonicBpms, [])
 
