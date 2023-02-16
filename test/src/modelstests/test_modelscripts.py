70e03414380cd6708d4186ca640963f4291f4daf
palonso <pablo.alonso@upf.edu>
2023-02-16 20:38:56 +0100

    Update test_modelscripts
    - now it can be executed from external  locations
    - it supports downloading the models from the web

--- test/src/modelstests/test_modelscripts.py
+++ test/src/modelstests/test_modelscripts.py
@@ -6,19 +6,17 @@ from pathlib import Path
 
 ESSENTIA_ROOT = Path(__file__).parent.parent.parent.parent.resolve()
 sys.path.append(str(ESSENTIA_ROOT / "src" / "examples" / "python" / "models"))
-sys.path.append(str(ESSENTIA_ROOT / "test" / "src" / "unittests"))
 
-from essentia_test import testdata
-from genereate_example_scripts import generate_example_scripts
+from generate_example_scripts import generate_example_scripts
 
 
 class TestModels(unittest.TestCase):
-    output_dir = ESSENTIA_ROOT / "build" / "script_models"
-    audio_file = str(Path(testdata.audio_dir, "recorded", "vignesh.wav"))
+    output_dir = ESSENTIA_ROOT / "build" / "model_scripts"
+    audio_file = ESSENTIA_ROOT / "test" / "audio" / "recorded" / "vignesh.wav"
 
     scripts = generate_example_scripts(
         output_dir,
-        audio_file=audio_file,
+        audio_file=str(audio_file),
         force=True,
         download_models=True,
     )
@@ -26,7 +24,7 @@ class TestModels(unittest.TestCase):
     def testScriptExecutes(self):
         for script in self.scripts:
             with self.subTest(i=script):
-                subprocess.run(["python3", script], check=True)
+                subprocess.check_call(["python3", script], cwd=str(Path(script).parent))
 
 
 if __name__ == "__main__":
