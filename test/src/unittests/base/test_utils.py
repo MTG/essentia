f646fc4e0e8a09fcbcf0406ce1a613c86c2f1be5
Xavier Lizarraga <xavilizarraga@gmail.com>
2024-05-17 18:01:02 +0200

    Add unittest for hz2midi()

--- test/src/unittests/base/test_utils.py
+++ test/src/unittests/base/test_utils.py
@@ -18,48 +18,49 @@
 # version 3 along with this program. If not, see http://www.gnu.org/licenses/
 
 
-
 from essentia_test import *
 import math
-import cmath # for asinh
+import cmath  # for asinh
 import sys
+
 
 class TestUtils(TestCase):
     def testIsSilent(self):
-        self.assertEqual(True, isSilent([0]*100))
+        self.assertEqual(True, isSilent([0] * 100))
 
     def testInstantPower(self):
         sample = list(range(1, 11))
         p = 0
         for s in sample:
             p += s**2
-        p /= float( len(sample) )
+        p /= float(len(sample))
 
         self.assertAlmostEqual(p, instantPower(sample))
 
     def testIsPowerOfTwo(self):
         self.assertTrue(isPowerTwo(0))
-        top =131072
-        k=1
+        top = 131072
+        k = 1
         while k < top:
             self.assertTrue(isPowerTwo(k))
-            k*=2
+            k *= 2
         while k < top:
-            k=2*k+1
+            k = 2 * k + 1
             self.assertTrue(not isPowerTwo(k))
 
     def testNextPowerOfTwo(self):
         self.assertEqual(nextPowerTwo(0), 0)
         self.assertEqual(nextPowerTwo(1), 1)
-        top =131072
-        k=2
+        top = 131072
+        k = 2
         lastPowerTwo = 2
         while k < top:
-            if not isPowerTwo(k): self.assertEqual(nextPowerTwo(k), 2*lastPowerTwo)
+            if not isPowerTwo(k):
+                self.assertEqual(nextPowerTwo(k), 2 * lastPowerTwo)
             else:
                 self.assertEqual(nextPowerTwo(k), k)
-                lastPowerTwo=k
-            k +=1
+                lastPowerTwo = k
+            k += 1
 
     def testLinToDb(self):
         lin = 12.34
@@ -68,8 +69,8 @@ class TestUtils(TestCase):
 
     def testDbToLin(self):
         db = -45.5
-        expected_lin = 10**(db/10.)
-        self.assertAlmostEqual(expected_lin, db2lin(db) , 5e-7)
+        expected_lin = 10 ** (db / 10.0)
+        self.assertAlmostEqual(expected_lin, db2lin(db), 5e-7)
 
     def testPowToDb(self):
         pow = 12.34
@@ -78,8 +79,8 @@ class TestUtils(TestCase):
 
     def testDbToPow(self):
         db = -45.5
-        expected_pow = 10**(db/10.)
-        self.assertAlmostEqual(expected_pow, db2pow(db) , 5e-7)
+        expected_pow = 10 ** (db / 10.0)
+        self.assertAlmostEqual(expected_pow, db2pow(db), 5e-7)
 
     def testAmpToDb(self):
         amp = 12.34
@@ -88,8 +89,8 @@ class TestUtils(TestCase):
 
     def testDbToAmp(self):
         db = -45.5
-        expected_amp = 10**(0.5*db/10.)
-        self.assertAlmostEqual(expected_amp, db2amp(db) , 5e-7)
+        expected_amp = 10 ** (0.5 * db / 10.0)
+        self.assertAlmostEqual(expected_amp, db2amp(db), 5e-7)
 
     def testBarkToHz(self):
         bark = 5
@@ -98,21 +99,26 @@ class TestUtils(TestCase):
 
     def testHzToBark(self):
         hz = 440
-        expected_bark = 26.81*hz / (1960 + hz) - 0.53
+        expected_bark = 26.81 * hz / (1960 + hz) - 0.53
         self.assertAlmostEqual(expected_bark, hz2bark(hz))
 
     def testMelToHz(self):
         mel = 5
-        expected_hz = 700.0*(math.exp(mel/1127.01048)-1.0)
+        expected_hz = 700.0 * (math.exp(mel / 1127.01048) - 1.0)
         self.assertAlmostEqual(expected_hz, mel2hz(mel))
 
     def testHzToMel(self):
         hz = 440
-        expected_mel = 1127.01048*math.log(hz/700.0+1.0)
+        expected_mel = 1127.01048 * math.log(hz / 700.0 + 1.0)
         self.assertAlmostEqual(expected_mel, hz2mel(hz))
+
+    def testHzToMidi(self):
+        hz = 440
+        expected_midi = 69
+        self.assertAlmostEqual(expected_midi, hz2midi(hz))
 
 
 suite = allTests(TestUtils)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     TextTestRunner(verbosity=2).run(suite)
