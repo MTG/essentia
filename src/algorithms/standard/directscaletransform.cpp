5816613f141359318c3265802ab3f285eeb4dea1
Anmol Mishra <anmolmishra1997@gmail.com>
2024-06-27 12:19:13 +0200

    Native implementation of direct scale transform

---
+++ src/algorithms/standard/directscaletransform.cpp
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2006-2021  Music Technology Group - Universitat Pompeu Fabra
+ *
+ * This file is part of Essentia
+ *
+ * Essentia is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <complex>
+using namespace std;
+
+#define FOR(i,l,r) for(int i=l; i<r; i++)
+
+vector<vector< complex<double> >> DirectScaleTransform(int N=10, int C=6, int fs=1) {
+    complex<double> zi = 1i;
+    double step = M_PI/log(N+1);
+    int num_rows = C/step;
+
+    vector<vector< complex<double> >> result(num_rows, vector< complex<double> >(N-1, 0));
+    double Ts = 1/fs;
+
+    FOR(i, 0, num_rows) {
+        FOR(j, 0, N-1) {
+            double c = step * i;
+            double k = j + 1;
+            complex<double> k_ = complex<double>(k * Ts);
+            complex<double> c_ = complex<double>(0.5) - zi * c; 
+
+            complex<double> M = pow(k_, c_)/(c_ * sqrt(2*M_PI));
+            result[i][j] = M;
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<vector< complex<double> >> result = DirectScaleTransform();
+    for(auto row: result){
+        for(auto elem: row){
+            cout << elem.real() << "+" << elem.imag() << "j ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
