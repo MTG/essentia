5b683b315c927eab95be0b723d467338b328c32a
Pablo Alonso <pablo.alonso@upf.edu>
2025-02-03 18:33:29 +0100

    Add new MAEST example scripts

---
+++ src/examples/python/models/scripts/classification-heads/genre_discogs400/genre_discogs400-discogs-maest-10s-pw-1_predictions.py
@@ -0,0 +1,12 @@
+from essentia import Pool
+from essentia.standard import MonoLoader, TensorflowPredictMAEST, TensorflowPredict
+
+audio = MonoLoader(filename="audio.wav", sampleRate=16000, resampleQuality=4)()
+embedding_model = TensorflowPredictMAEST(graphFilename="discogs-maest-10s-pw-2.pb", output="PartitionedCall/Identity_12")
+embeddings = embedding_model(audio)
+
+pool = Pool()
+pool.set("embeddings", embeddings)
+
+model = TensorflowPredict(graphFilename="genre_discogs400-discogs-maest-10s-pw-1.pb", inputs=["embeddings"], outputs=["PartitionedCall/Identity_1"])
+predictions = model(pool)["PartitionedCall/Identity_1"]
